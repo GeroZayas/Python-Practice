df6f1eaba240ccd515732fb6e19597b3da90a469
Gerardo Zayas <77191089+GeroZayas@users.noreply.github.com>
2022-03-04 22:45:38 +0100

    Add files via upload

---
+++ 16_The_100_Doors_RosettaCode.py
@@ -0,0 +1,9 @@
+# 100 doors
+# from https://rosettacode.org/wiki/100_doors
+
+for i in range(1, 101):
+    if i ** 0.5 % 1:
+        door_state = 'closed'
+    else:
+        door_state = 'open'
+    print("Door {}:{}".format(i, door_state))
