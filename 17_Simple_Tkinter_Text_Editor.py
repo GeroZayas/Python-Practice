01ee50aa9579c5faa9b3fea9efca18614231d874
Gerardo Zayas <77191089+GeroZayas@users.noreply.github.com>
2022-03-06 00:11:42 +0100

    Create New File Function

--- 17_Simple_Tkinter_Text_Editor.py
+++ 17_Simple_Tkinter_Text_Editor.py
@@ -5,9 +5,16 @@ from tkinter import filedialog
 from tkinter import font
 
 root = Tk()
-root.title("Gero Zayas Text Editor")
+root.title("Gero's Text Editor")
 root.iconbitmap("./text.ico")
 root.geometry("1200x660")
+
+
+# Create new file function
+def new_file():
+    my_text.delete("1.0", END)
+    root.title('New File - TextPad!')
+    status_bar.config(text="New File        ")
 
 # Create a main frame
 my_frame = Frame(root)
@@ -33,16 +40,17 @@ my_menu = Menu(root)
 root.config(menu=my_menu)
 
 # add File Menu
-file_menu = Menu(my_menu)
+file_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label='File', menu=file_menu)
-file_menu.add_command(label="New")
+file_menu.add_command(label="New", command=new_file)
 file_menu.add_command(label="Open")
 file_menu.add_command(label="Save")
+file_menu.add_command(label="Save as")
 file_menu.add_separator()
-file_menu.add_command(label="Exit")
+file_menu.add_command(label="Exit", command=quit)
 
 # Add Edit Menu
-edit_menu = Menu(my_menu)
+edit_menu = Menu(my_menu, tearoff=False)
 my_menu.add_cascade(label='Edit', menu=edit_menu)
 edit_menu.add_command(label="Cut")
 edit_menu.add_command(label="Copy")
@@ -50,7 +58,7 @@ edit_menu.add_command(label="Undo")
 edit_menu.add_command(label="Redo")
 
 # Add Status Bar to bottom of App
-status_bar = Label(root, text="Ready    ", anchor=E)
+status_bar = Label(root, text="Ready        ", anchor=E)
 status_bar.pack(fill=X, side=BOTTOM, ipady=5)
 
 root.mainloop()
