31439be1f2e0222112173727500dfd7401287923
GeroZayas <gerozayas@gmail.com>
2022-03-11 10:18:59 +0100

    Update

--- 19_learn_about_with_iter_and_modules.py
+++ 19_learn_about_with_iter_and_modules.py
@@ -1,5 +1,6 @@
 import platform as pl
 
+
 # Using iter() and next()
 new_tuple = ("gero", "gupa", "camtu")
 
@@ -16,3 +17,4 @@ print(x)
 # Using module platform to get info about the system and more
 sys_info = pl.system(), pl.architecture(), pl.python_version_tuple()  # ('Windows', ('64bit', 'WindowsPE'), ('3', '10', '2'))
 print(sys_info)
+
---
+++ 20_JSON_practice.py
@@ -0,0 +1,15 @@
+import json
+import json
+
+# a Python object (dict):
+x = {
+    "name": "Gero",
+    "age": 30,
+    "city": "Barcelona"
+}
+
+# convert into JSON:
+y = json.dumps(x)
+
+# the result is a JSON string:
+print(y)
