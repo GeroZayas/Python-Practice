b793f92293ab6a482e06c90bc2c0ce41a01ae4a7
GeroZayas <gerozayas@gmail.com>
2022-03-11 10:24:17 +0100

    Update

--- 20_JSON_practice.py
+++ 20_JSON_practice.py
@@ -12,3 +12,12 @@ y = json.dumps(x)
 
 # the result is a JSON string:
 print(y)
+
+print(json.dumps(["apple", "bananas"]))  # list
+print(json.dumps(("apple", "bananas")))  # tuple
+print(json.dumps("hello"))  # string
+print(json.dumps(42))  # int
+print(json.dumps(31.76))  # float
+print(json.dumps(True))  # boolean
+print(json.dumps(False))  # boolean
+print(json.dumps(None))  # None
