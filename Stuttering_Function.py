c86ebeb836ab133c827ca9aa766a915bbb7b43dc
Gerardo Zayas <77191089+GeroZayas@users.noreply.github.com>
2022-03-15 12:29:53 +0100

    Add files via upload

---
+++ Stuttering_Function.py
@@ -0,0 +1,24 @@
+"""
+Stuttering Function
+Publicado por Deep Xavier 
+
+Write a function that stutters a word as if someone is struggling to read it. The first two letters are repeated twice with an ellipsis ... and space after each, and then the word is pronounced with a question mark ?.
+
+Examples
+stutter("incredible") ➞ "in... in... incredible?"
+
+stutter("enthusiastic") ➞ "en... en... enthusiastic?"
+
+stutter("outstanding") ➞ "ou... ou... outstanding?"
+Notes
+Assume all input is in lower case and at least two characters long.
+"""
+
+
+def stutter(word):
+    first_two_letters = word[:2]
+    final_string = (f"{first_two_letters}... " * 2) + word + "?"
+    return final_string
+
+
+print(stutter("outstanding"))
