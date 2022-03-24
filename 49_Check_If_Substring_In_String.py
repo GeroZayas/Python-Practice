5a7bd41d39fb7570fe33ea14adc0b1ce9d9747dd
GeroZayas <gerozayas@gmail.com>
2022-03-24 11:35:52 +0100

    Update

---
+++ 49_Check_If_Substring_In_String.py
@@ -0,0 +1,47 @@
+"""
+5. Write a Python program to check the nth-1 string is a proper substring of nth string in a given list of strings.
+
+Input:
+['a', 'abb', 'sfs', 'oo', 'de', 'sfde']
+Output:
+True
+
+Input:
+['a', 'abb', 'sfs', 'oo', 'ee', 'sfde']
+Output:
+False
+
+Input:
+['a', 'abb', 'sad', 'ooaaesdfe', 'sfsdfde', 'sfsd', 'sfsdf', 'qwrew']
+Output:
+False
+
+Input:
+['a', 'abb', 'sad', 'ooaaesdfe', 'sfsdfde', 'sfsd', 'sfsdf', 'qwsfsdfrew']
+Output:
+True
+"""
+
+
+def check_substrings(string_list) -> bool:
+    for string in string_list[:-1]:
+        if (string) in string_list[-1]:
+            return True
+    return False
+
+
+print(check_substrings(["a", "abb", "sfs", "oo", "de", "sfde"]))  # True
+
+print(check_substrings(["a", "abb", "sfs", "oo", "ee", "sfde"]))  # False
+
+print(
+    check_substrings(
+        ["a", "abb", "sad", "ooaaesdfe", "sfsdfde", "sfsd", "sfsdf", "qwrew"]
+    )
+)  # False
+
+print(
+    check_substrings(
+        ["a", "abb", "sad", "ooaaesdfe", "sfsdfde", "sfsd", "sfsdf", "qwsfsdfrew"]
+    )
+)  # True
