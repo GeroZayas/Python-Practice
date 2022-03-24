a6428b45ed292fd338957121320cdadb67948a44
Gerardo Zayas <77191089+GeroZayas@users.noreply.github.com>
2022-03-24 09:43:17 +0100

    Update 48_Stone_Piles_Exercise.py

--- 48_Stone_Piles_Exercise.py
+++ 48_Stone_Piles_Exercise.py
@@ -23,3 +23,26 @@ Input: 17
 Output:
 [17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
 """
+
+# TODO check if input is even or odd
+# If even, all piles must have an even number of stones.
+
+the_inputs = [2, 10, 3, 17]
+
+
+# TODO print a list with the piles
+
+
+def print_piles(the_amount):
+    pile = []
+    for x in range(the_amount):
+        pile.append(the_amount)
+        the_amount += 2
+    return pile
+
+
+# IMPORTANT: Notice tha t when we call the function in map we don't use the parentheses ()
+all_together = list(map(print_piles, the_inputs))
+
+for element in all_together:
+    print(" *** ", element)
