33ee68dff00347037ae27174ce5eba3ee2662898
GeroZayas <gerozayas@gmail.com>
2022-03-25 10:07:30 +0100

    Update

---
+++ 52.py
@@ -0,0 +1,49 @@
+"""
+https://www.w3resource.com/python-exercises/puzzles/index.php#EDITOR
+8. Write a Python program to split a string of words separated by commas and spaces into two lists, words and separators.
+
+Input: W3resource Python, Exercises.
+
+Output:
+[['W3resource', 'Python', 'Exercises.'], [' ', ', ']]
+
+Input: The dance, held in the school gym, ended at midnight.
+
+Output:
+[['The', 'dance', 'held', 'in', 'the', 'school', 'gym', 'ended', 'at', 'midnight.'], [' ', ', ', ' ', ' ', ' ', ' ', ', ', ' ', ' ']]
+
+Input: The colors in my studyroom are blue, green, and yellow.
+
+Output:
+[['The', 'colors', 'in', 'my', 'studyroom', 'are', 'blue', 'green', 'and', 'yellow.'], [' ', ' ', ' ', ' ', ' ', ' ', ', ', ', ', ' ']]
+"""
+first_input = "W3resource Python, Exercises."
+second_input = "The dance, held in the school gym, ended at midnight."
+third_input = "The colors in my studyroom are blue, green, and yellow."
+
+import re
+
+
+# def split_words_separators(an_input: str) -> list:
+#     """Returns two lists, one with words, and one with the separators used in input string"""
+#     split_list = re.split(" |,", an_input)
+#     words = []
+#     separators = []
+#     for element in split_list:
+#         if str(element.isalpha()):
+#             words.append(element)
+#         else:
+#             separators.append(element)
+#     final_list_of_lists = [words, separators]
+#     return final_list_of_lists
+
+
+def split_words_separators(an_input: str) -> list:
+    """Returns two lists, one with words, and one with the separators used in input string"""
+    words = []
+    separators = []
+    print(words)
+    print(separators)
+
+
+print(split_words_separators(first_input))
