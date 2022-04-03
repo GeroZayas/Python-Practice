170a53c40823da5659ab36b4e9dd61ca11b43fcd
GeroZayas <gerozayas@gmail.com>
2022-04-03 12:08:04 +0200

    Inital commit

---
+++ 55_Beeramid_Codewars.py
@@ -0,0 +1,15 @@
+def beeramid(bonus, price):
+    levels = 0
+    beers = int(bonus // price)
+    print("beers", beers)
+    if beers == 4:
+        levels = 1
+    elif beers > 4:
+        levels = 2
+    print("levels", levels)
+    return levels
+
+
+print(beeramid(9, 2))  # 1
+print(beeramid(21, 1.5))  # 3
+print(beeramid(-1, 4))  # 0
