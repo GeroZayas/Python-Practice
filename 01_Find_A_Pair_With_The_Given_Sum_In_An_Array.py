69a93d92b3b906965923423808b41ec790ac6fd3
GeroZayas <gerozayas@gmail.com>
2022-09-14 21:35:58 +0200

    Update

--- 01_Find_A_Pair_With_The_Given_Sum_In_An_Array.py
+++ 01_Find_A_Pair_With_The_Given_Sum_In_An_Array.py
@@ -2,9 +2,10 @@
 nums = [8, 7, 2, 5, 3, 1]
 target = 10
 
+
 def find_pair_for_sum(array, target):
-    pair  = []
-    list_of_pairs = []  
+    pair = []
+    list_of_pairs = []
     # For loop that takes first num in array and checks if
     # there is another number with which, if added together,
     # they yield target (example, if target = 10, and
@@ -20,7 +21,7 @@ def find_pair_for_sum(array, target):
     # if there are no pairs:
     if len(pair) == 0:
         print("Pair  not found")
-    #if there are pairs:
+    # if there are pairs:
     elif len(pair) > 0:
         # I set this counter so I can use it as index points to print the pairs that yield the target sum
         counter = 0
@@ -28,8 +29,11 @@ def find_pair_for_sum(array, target):
             # I set it counter < len(pair) so it does not yield a out of index error
             while counter < len(pair):
                 print("Pair found ", f"({pair[counter]}, {pair[counter+1]})")
-                counter += 2 # to move two indexes and print the next pair
-                if counter < len(pair): # so at the end it doesn't print another "or" after the last pair
+                counter += 2  # to move two indexes and print the next pair
+                if counter < len(
+                    pair
+                ):  # so at the end it doesn't print another "or" after the last pair
                     print("or")
-    
+
+
 find_pair_for_sum(nums, target)
