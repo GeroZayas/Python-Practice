3cae904ae8b5f7bd03a24d60cdca91eeed3c2116
GeroZayas <gerozayas@gmail.com>
2022-09-23 12:39:26 +0200

    add functions

--- 57_Speech_To_Text.py
+++ 57_Speech_To_Text.py
@@ -3,4 +3,18 @@ import pyttsx3
 import speech_recognition as sr
 import os
 
-engine = pyttsx3.init
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty("voices")
+
+# print(voices[1].id)
+
+engine.setProperty("voice", voices[0].id)
+
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def get():
+    r = sr.Recognizer()
