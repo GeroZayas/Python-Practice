a3866805886a57b9e611c9346aa5c8258b0a6737
GeroZayas <gerozayas@gmail.com>
2022-09-27 19:11:02 +0200

    Initial

---
+++ 58 Even or Odd.py
@@ -0,0 +1,12 @@
+# Even or Odd
+
+
+def even_or_odd(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+
+print(even_or_odd(9))  # 0dd
+print(even_or_odd(8))  # Even
