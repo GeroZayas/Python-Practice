4642f949922b7b18ff13959c010b2524d39ceaa1
GeroZayas <gerozayas@gmail.com>
2022-09-28 09:27:23 +0200

    create items

--- 59 General practice.py
+++ 59 General practice.py
@@ -1,6 +1,19 @@
+mylist1 = [1, "gero", 5.6, "zayas"]
+print(mylist1)
+
 # I create an empty list here ->
 mylist2 = list()
 
 print(mylist2)  # []
 
 print(type(mylist2))  # <class 'list'>
+
+# access items from mylist1
+item1, item2, item3, item4 = mylist1[0], mylist1[1], mylist1[2], mylist1[3]
+
+print(item1)
+print(item2)
+print(item3)
+print(item4)
+
+for x
