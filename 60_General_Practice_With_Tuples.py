bf7c6a1cdd90e30384c42654d1907aaba501195d
GeroZayas <gerozayas@gmail.com>
2022-09-28 23:28:12 +0200

    add method to timeit

--- 60_General_Practice_With_Tuples.py
+++ 60_General_Practice_With_Tuples.py
@@ -1,4 +1,5 @@
-from traceback import print_tb
+import sys
+import timeit
 
 
 my_tuple = (
@@ -8,6 +9,9 @@ my_tuple = (
 )  # the parentheses are optional, if you take it away, it is still a tuple
 
 print(my_tuple)
+
+print("\n" + "*" * 25)  # LINE BREAK
+
 
 # ----------------------------------------------------
 # TUPLES are INMUTABLE you can not change items like in a list
@@ -23,10 +27,15 @@ print(type(my_tuple_b))  # <class 'tuple'>
 item = my_tuple[0]
 print(item)  # Gero
 
+print("\n" + "*" * 25)  # LINE BREAK
+
 # ----------------------------------------------------
 
 for i in my_tuple:
     print(i)
+
+print("\n" + "*" * 25)  # LINE BREAK
+
 
 # ----------------------------------------------------
 if "Gero" in my_tuple:
@@ -34,8 +43,14 @@ if "Gero" in my_tuple:
 else:
     print("No")
 
+print("\n" + "*" * 25)  # LINE BREAK
+
+
 # ----------------------------------------------------
 print(len(my_tuple))  # 3
+
+print("\n" + "*" * 25)  # LINE BREAK
+
 
 # ----------------------------------------------------
 # we can count how many times an item is in a tuple with .count()
@@ -45,11 +60,43 @@ my_letter_tuple = ("a", "b", "a", "c", "a")
 print(my_letter_tuple)
 print(my_letter_tuple.count("a"))  # 3
 
+print("\n" + "*" * 25)  # LINE BREAK
+
+
 # ----------------------------------------------------
 # Make a list from a tuple with list()
+
 
 my_letter_list = list(my_letter_tuple)
 print(my_letter_list)  # ['a', 'b', 'a', 'c', 'a']
 
+print("\n" + "*" * 25)  # LINE BREAK
+
+
 # ----------------------------------------------------
-#
+# Compare lists and tuples
+# sys is imported for this
+
+
+print(sys.getsizeof(my_letter_tuple), "bytes")  # 80 bytes
+print(sys.getsizeof(my_letter_list), "bytes")  # 96 bytes
+
+print("\n" + "*" * 25)  # LINE BREAK
+
+
+# ----------------------------------------------------
+# it can be more efficient to iterate over a tuple
+# we import timeit to compare
+
+print("\n" + "*" * 25)  # LINE BREAK
+
+print(timeit.timeit(stmt="[0,1,2,3,4,5]", number=1000000))  # 0.1059750679996796
+print(timeit.timeit(stmt="(0,1,2,3,4,5)", number=1000000))  # 0.02168168599746423
+
+# it takes longer to create the list than the tuple
+
+# ----------------------------------------------------
+# it can be more efficient to iterate over a tuple
+# we import timeit to compare
+
+print("\n" + "*" * 25)  # LINE BREAK
