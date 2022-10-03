db5e56cbc4c2d19f5b5d38b947bd9c8a2ccc71c2
GeroZayas <gerozayas@gmail.com>
2022-10-03 23:45:12 +0200

    update

--- 68 Web Scraping.py
+++ 68 Web Scraping.py
@@ -16,17 +16,28 @@ soup = BeautifulSoup(web_page, "html.parser")
 
 song_title = soup.find(name="h1", attrs={"class": "titol-pagina"})
 
-print("Song: ", song_title.text)
+# print("Song: ", song_title.text)
 
 group_or_singer = soup.find(name="h2", attrs={"class": "subtitol-pagina"})
 
-print("Group:", group_or_singer.text)
+# print("Group:", group_or_singer.text)
 
 lyrics = soup.find_all(
     name="div", attrs={"class": "enc-lletra__lletra user-text seleccionable"}
 )
 
-for lyric in lyrics:
-    print(lyric.text)
 
-phrases_text = []
+song_lyrics = []
+song_lyrics.append(song_title.text)
+song_lyrics.append(group_or_singer.text)
+
+# print(song_lyrics)
+
+for lyric in lyrics:
+    song_lyrics.append(lyric.text)
+
+with open(f"{song_title.text} from {group_or_singer.text}.txt", "w") as lyrics_file:
+    for line in song_lyrics:
+        lyrics_file.write(line)
+
+print("Done")
