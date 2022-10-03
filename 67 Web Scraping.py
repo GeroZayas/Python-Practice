98106de058c98ce1dddcd0bb66828f2a72bbd1d8
GeroZayas <gerozayas@gmail.com>
2022-10-03 12:59:20 +0200

    Update name

--- 67 Web Scraping.py
+++
@@ -1,8 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import csv
-
-response = requests.get("https://www.cursdecatala.com/ca/frases-fetes/")
-curs_cat_frases_fetes = response.text
-
-soup = BeautifulSoup(curs_cat_frases_fetes, "html.parser")
