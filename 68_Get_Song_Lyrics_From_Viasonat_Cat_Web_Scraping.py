7492a8f8e57afa2da08ef223a3e8599b59fdbdf3
GeroZayas <gerozayas@gmail.com>
2022-10-04 00:31:10 +0200

    update comments

--- 68_Get_Song_Lyrics_From_Viasonat_Cat_Web_Scraping.py
+++ 68_Get_Song_Lyrics_From_Viasonat_Cat_Web_Scraping.py
@@ -2,39 +2,46 @@ from bs4 import BeautifulSoup
 import requests
 import csv
 
+# The User Inserts here a valid viasona.cat link of a song lyric
+
 print("Insert Web Link from Viasona.Cat:\n")
 
 WEB = input()
 
-# GOAL Get lyrics from this page
 
 response = requests.get(WEB)
 web_page = response.text
 soup = BeautifulSoup(web_page, "html.parser")
 # print(soup.prettify)
 
+# We get hold of the title here
 song_title = soup.find(name="h1", attrs={"class": "titol-pagina"})
 
 # print("Song: ", song_title.text)
 
+# We get hold of the group name or singer here
 group_or_singer = soup.find(name="h2", attrs={"class": "subtitol-pagina"})
 
 # print("Group:", group_or_singer.text)
 
+# We get hold of the lyrics here
 lyrics = soup.find_all(
     name="div", attrs={"class": "enc-lletra__lletra user-text seleccionable"}
 )
 
-
+# Declare a new list and append name of song and group
 song_lyrics = []
 song_lyrics.append("Title: " + song_title.text + "\n\n")
 song_lyrics.append("Group / Singer: " + group_or_singer.text + "\n\n")
 
 # print(song_lyrics)
 
+
+# We now append all the lyrics, line by line
 for lyric in lyrics:
     song_lyrics.append(lyric.text)
 
+# We create a new .txt file and add the lyrics to it
 with open(f"{song_title.text} from {group_or_singer.text}.txt", "w") as lyrics_file:
     for line in song_lyrics:
         lyrics_file.write(line)
