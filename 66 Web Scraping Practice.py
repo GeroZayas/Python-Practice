6573522d788ce5987644438b512fbb9fc585386f
GeroZayas <gerozayas@gmail.com>
2022-10-03 12:24:46 +0200

    Update

--- 66 Web Scraping Practice.py
+++ 66 Web Scraping Practice.py
@@ -12,29 +12,44 @@ soup = BeautifulSoup(catala_com_cal_web_page, "html.parser")
 # section_heading = soup.find(name='h3', class_='heading')
 # print(section_heading.get("class"))
 
-correct_phrases = soup.find_all(name="span", style="color: #3333ff;")
-correct_phrases_text = []
+# correct_phrases = soup.find_all(name="span", style="color: #3333ff;")
+# correct_phrases_text = []
 
-wrong_phrases = soup.find_all(name="span", style="color: #cc0000;")
-wrong_phrases_text = []
+# wrong_phrases = soup.find_all(name="span", style="color: #cc0000;")
+# wrong_phrases_text = []
+
+phrases = soup.find_all(attrs={"style": "color: #cc0000;"})
+phrases_text = []
+
+# print(phrases)
 
 
 # print(correct_phrases)
 
-for phrase in correct_phrases:
-    text = phrase.getText()
-    correct_phrases_text.append(text)
+# for phrase in correct_phrases:
+#     text = phrase.getText()
+#     correct_phrases_text.append(text)
 
-for phrase in wrong_phrases:
+# for phrase in wrong_phrases:
+#     text = phrase.getText()
+#     wrong_phrases_text.append(text)
+
+for phrase in phrases:
     text = phrase.getText()
-    wrong_phrases_text.append(text)
+    phrases_text.append(text)
 
 # print(correct_phrases_text)
 
 filename = "catalan_phrases.txt"
 
+# with open(filename, mode="w", newline="") as file:
+#     # for phrase in correct_phrases_text:
+#     #     file.write(phrase + "\n")
+#     for wrong_phrase in wrong_phrases_text:
+#         file.write(wrong_phrase + "\n")
+
 with open(filename, mode="w", newline="") as file:
-    for phrase in correct_phrases_text:
+    for phrase in phrases_text:
         file.write(phrase + "\n")
 
 
