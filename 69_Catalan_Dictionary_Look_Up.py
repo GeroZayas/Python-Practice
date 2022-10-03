39b756ecffdba223b9a51ff23ed03856a9d80431
GeroZayas <gerozayas@gmail.com>
2022-10-04 01:17:06 +0200

    update

--- 69_Catalan_Dictionary_Look_Up.py
+++ 69_Catalan_Dictionary_Look_Up.py
@@ -8,12 +8,26 @@ import requests
 
 # The User Inserts a word here
 
-print("Insert Catalan Word:\n")
+# print("Insert Catalan Word:\n")
 
-WORD = input()
+# WORD = input()
 
 
-response = requests.get(WORD)
+# response = requests.get(f"https://www.wordreference.com/definicio/{WORD}")
+response = requests.get(f"https://www.wordreference.com/definicio/amor")
 word_definition = response.text
 soup = BeautifulSoup(word_definition, "html.parser")
-print(soup.prettify)
+# print(soup.prettify)
+
+word_definition = soup.find_all(name="div", attrs={"class": "entryLa trans clickable"})
+
+word_definition_list = []
+
+for text in word_definition:
+    word_definition_list.append(text.ol.li.get_text)
+
+with open("definition.txt", "w") as def_file:
+    for line in word_definition_list:
+        def_file.write(str(line))
+
+print("Done")
