6625a2b0cc7cf1f0f841549745f22f2ef66182b9
GeroZayas <gerozayas@gmail.com>
2022-10-04 02:40:59 +0200

    Update

--- 69_Catalan_Dictionary_Look_Up.py
+++ 69_Catalan_Dictionary_Look_Up.py
@@ -24,19 +24,30 @@ soup = BeautifulSoup(word_definition, "html.parser")
 # word_definition = soup.find_all(name="div", attrs={"class": "entryLa trans clickable"})
 
 
+words = soup.find_all("span", class_="lemmaLa main")
+word_list = []
+
+for word in words:
+    word_list.append(word.text)
+
+# print("This is the word list->", word_list)
+
 word_definition = soup.find_all("ol", class_="olLa")
 
 word_definition_list = []
 
 for text in word_definition:
-    word_definition_list.append(text.text)
+    word_definition_list.append(str(text.text.split(".")).strip("[]"))
 
-for line in word_definition_list:
-    print("******", line)
+# for line in word_definition_list:
+#     print(line)
 
 
-with open(f"definition_of_{WORD}.txt", "w") as def_file:
+with open(f"Looked Up Catalan words.txt", "a") as def_file:
+    for word in word_list:
+        def_file.write("***" + str(word).upper() + "***" + "\n")
     for line in word_definition_list:
-        def_file.write(str(line))
+        def_file.write(str(line) + "\n")
+    def_file.write("-" * 70 + "\n\n")
 
 print("Done")
