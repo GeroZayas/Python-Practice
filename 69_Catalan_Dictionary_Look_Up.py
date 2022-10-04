8ca2ffbfaa49bddcd0713c5b89ab65d869bcf0fa
GeroZayas <gerozayas@gmail.com>
2022-10-04 02:05:22 +0200

    update

--- 69_Catalan_Dictionary_Look_Up.py
+++ 69_Catalan_Dictionary_Look_Up.py
@@ -8,25 +8,34 @@ import requests
 
 # The User Inserts a word here
 
-# print("Insert Catalan Word:\n")
+print("Insert Catalan Word:\n")
 
-# WORD = input()
+WORD = input()
 
 
-# response = requests.get(f"https://www.wordreference.com/definicio/{WORD}")
-response = requests.get(f"https://www.wordreference.com/definicio/amor")
+response = requests.get(f"https://www.wordreference.com/definicio/{WORD}")
+
+# response = requests.get(f"https://www.wordreference.com/definicio/amor")
+
 word_definition = response.text
 soup = BeautifulSoup(word_definition, "html.parser")
 # print(soup.prettify)
 
-word_definition = soup.find_all(name="div", attrs={"class": "entryLa trans clickable"})
+# word_definition = soup.find_all(name="div", attrs={"class": "entryLa trans clickable"})
+
+
+word_definition = soup.find_all("ol", class_="olLa")
 
 word_definition_list = []
 
 for text in word_definition:
-    word_definition_list.append(text.ol.li.get_text)
+    word_definition_list.append(text.text)
 
-with open("definition.txt", "w") as def_file:
+for line in word_definition_list:
+    print("******", line)
+
+
+with open(f"definition_of_{WORD}.txt", "w") as def_file:
     for line in word_definition_list:
         def_file.write(str(line))
 
