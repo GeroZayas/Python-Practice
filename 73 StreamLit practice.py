759717ae8cced3230feb07fe61e6c0563a5f4d47
GeroZayas <gerozayas@gmail.com>
2022-10-06 08:50:01 +0200

    update

--- 73 StreamLit practice.py
+++ 73 StreamLit practice.py
@@ -4,6 +4,33 @@ import pandas as pd
 st.write(
     """
          # My First App
-         Hello *world!*
+         ## Hello *world!* 
+         This is very **important** for me
+         > I'm learning a lot
+         1. One
+         2. Two
+         3. Three
+         [Google](https://www.google.com)
          """
+)
+
+st.write(
+    """
+         | Syntax | Description |
+| ----------- | ----------- |
+| Header | Title |
+| Paragraph | Text |
+
+term
+: definition
+
+- [x] Write the press release
+- [ ] Update the website
+- [ ] Contact the media
+
+That is so funny! :joy:
+
+I need to highlight these ==very important words==.
+
+"""
 )
