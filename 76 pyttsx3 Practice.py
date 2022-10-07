9d01b8f0f26ea41e6a8071d19173241a30b3d2c1
GeroZayas <gerozayas@gmail.com>
2022-10-07 20:31:53 +0200

    update

--- 76 pyttsx3 Practice.py
+++ 76 pyttsx3 Practice.py
@@ -5,7 +5,7 @@ engine = pyttsx3.init()  # object creation
 """ RATE"""
 rate = engine.getProperty("rate")  # getting details of current speaking rate
 print(rate)  # printing current voice rate
-engine.setProperty("rate", 125)  # setting up new voice rate
+engine.setProperty("rate", 190)  # setting up new voice rate
 
 
 """VOLUME"""
@@ -13,7 +13,7 @@ volume = engine.getProperty(
     "volume"
 )  # getting to know current volume level (min=0 and max=1)
 print(volume)  # printing current volume level
-engine.setProperty("volume", 1.5)  # setting up volume level  between 0 and 1
+engine.setProperty("volume", 1)  # setting up volume level  between 0 and 1
 
 """VOICE"""
 voices = engine.getProperty("voices")  # getting details of current voice
@@ -22,8 +22,14 @@ engine.setProperty(
     "voice", voices[1].id
 )  # changing index, changes voices. 1 for female
 
-engine.say("Gero!")
-engine.say("What is up? my man.")
+
+with open("text_leer.txt", "r") as text:
+    for line in text:
+        engine.say(line)
+        print(line)
+
+# engine.say("Gero!")
+# engine.say("What is up? my man.")
 engine.runAndWait()
 engine.stop()
 
