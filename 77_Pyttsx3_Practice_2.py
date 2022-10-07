85a4985fd6e6ba0de554dddbef44fd37a251323b
GeroZayas <gerozayas@gmail.com>
2022-10-07 21:42:05 +0200

    update

--- 77_Pyttsx3_Practice_2.py
+++ 77_Pyttsx3_Practice_2.py
@@ -1,46 +1,43 @@
 import pyttsx3
 
-engine = pyttsx3.init()  # object creation
+engine = pyttsx3.init()
 
 """ RATE"""
 rate = engine.getProperty("rate")  # getting details of current speaking rate
 print(rate)  # printing current voice rate
 engine.setProperty("rate", 190)  # setting up new voice rate
 
-
 """VOLUME"""
 volume = engine.getProperty(
     "volume"
 )  # getting to know current volume level (min=0 and max=1)
 print(volume)  # printing current volume level
-engine.setProperty("volume", 1)  # setting up volume level  between 0 and 1
+
+voices_in_pc = {
+    "Spanish EU": 0,
+    "English Ame": 1,
+    "Spanish LA": 2,
+    "Portuguese Br": 3,
+}
 
 """VOICE"""
 voices = engine.getProperty("voices")  # getting details of current voice
 
 engine.setProperty(
-    "voice", voices[0].id
+    "voice", voices[voices_in_pc["Spanish LA"]].id
 )  # changing index, changes voices. 1 for female
 
-text_string = ""
+to_be_said = input("Insert speech: ")
 
+print(type(to_be_said))
 
-# OPEN TEXT FILE TO READ FROM --------
-with open("./news_scraped/news_from_rtve.txt", "r") as text:
-    for line in text:
-        # engine.say(line)
-        text_string += line + "\n"
-
-print(text_string)
-# ------------------------------------
-
-
-# engine.say("Gero!")
-# engine.say("What is up? my man.")
+engine.say(to_be_said)
 engine.runAndWait()
 engine.stop()
 
+name_recording = input("Insert name of recording: ")
+
 """Saving Voice to a file"""
 # On linux make sure that 'espeak' and 'ffmpeg' are installed
-engine.save_to_file(text_string, "test.mp3")
+engine.save_to_file(to_be_said, f"{name_recording}.mp3")
 engine.runAndWait()
