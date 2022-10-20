ecc734cd336cb3cca242cb375c1fa5e0e524fdcb
GeroZayas <gerozayas@gmail.com>
2022-10-20 07:37:42 +0200

    update

--- 86_Practice_With_Prettytable.py
+++ 86_Practice_With_Prettytable.py
@@ -2,13 +2,18 @@ from prettytable import PrettyTable
 
 pt = PrettyTable()
 
-pt.field_names = ["name", "profession", "age"]
+pt.field_names = ["name", "profession", "age", "comments"]
 
 names = ["Gero", "Mar", "Elisa"]
 professions = ["Python Programmer", "HHRR", "Doctor"]
 ages = [31, 31, 56]
+comments = [
+    "This guy is amazing",
+    "This is my girlfriend and I love her",
+    "This is my mother and she's literally the best",
+]
 
-for name, profession, age in zip(names, professions, ages):
-    pt.add_row([name, profession, age])
+for name, profession, age, comment in zip(names, professions, ages, comments):
+    pt.add_row([name, profession, age, comment])
 
 print(pt)
