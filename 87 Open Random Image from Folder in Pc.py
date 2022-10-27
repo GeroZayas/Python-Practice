94b58bc5a3447975030bee74c632a927d67b3e98
GeroZayas <gerozayas@gmail.com>
2022-10-28 00:22:45 +0200

    add code

--- 87 Open Random Image from Folder in Pc.py
+++ 87 Open Random Image from Folder in Pc.py
@@ -1,1 +1,17 @@
-#
+import random, os
+from PIL import Image
+
+path = r"C:\Users\Gero Zayas\Downloads\Images\PinDown__Quotes"
+
+
+random_filename = random.choice(
+    [x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))]
+)
+print(random_filename)
+
+
+# read the image
+im = Image.open(f"{path}/{random_filename}")
+
+# show image
+im.show()
