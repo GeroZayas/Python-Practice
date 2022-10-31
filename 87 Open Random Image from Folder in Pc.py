485702e9d4b3dd4009bac30c2799d11aa96b3387
GeroZayas <gerozayas@gmail.com>
2022-10-31 09:04:32 +0100

    Change path to input from user

--- 87 Open Random Image from Folder in Pc.py
+++ 87 Open Random Image from Folder in Pc.py
@@ -1,12 +1,16 @@
 import random, os
 from PIL import Image
 
-path = r"C:\Users\Gero Zayas\Downloads\Images\PinDown__Quotes"
+
+# path = r"C:\Users\Gero Zayas\Downloads\Images\PinDown__Quotes"
+
+path = input("Insert your path here: ")
 
 
 random_filename = random.choice(
     [x for x in os.listdir(path) if os.path.isfile(os.path.join(path, x))]
 )
+
 print(random_filename)
 
 
