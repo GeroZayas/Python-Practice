44ad1bb113c4aa1741a98eda643a5f8aebf4efc2
GeroZayas <gerozayas@gmail.com>
2022-11-14 12:03:53 +0100

    Update

--- 100 multiprocessing practice.py
+++ 100 multiprocessing practice.py
@@ -1,3 +1,41 @@
 # 100 multiprocessing practice.py
 from multiprocessing import Process
 import os
+import sys
+import time
+
+
+def square_nums():
+    result = 0
+    for i in range(0, 100):
+        result += i * i
+        time.sleep(0.1)
+
+    return result
+
+
+print(square_nums())
+
+processes = []
+num_processes = os.cpu_count()  # 4
+
+print(num_processes)
+
+
+# create processes
+for i in range(num_processes):
+    p = Process(target=square_nums)
+    processes.append(p)
+
+
+# start
+for p in processes:
+    p.start()
+
+# join
+
+for p in processes:
+    p.join()
+
+
+print("End Main")
