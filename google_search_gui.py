7f869cdb8cfb06eec8e363998388bc908af9f5fa
GeroZayas <gerozayas@gmail.com>
2022-11-17 08:01:40 +0100

    Add Youtube look up

--- google_search_gui.py
+++ google_search_gui.py
@@ -3,16 +3,25 @@ import PySimpleGUI as sg
 import webbrowser
 
 
-sg.theme("DarkBlue7")
+sg.theme("DarkAmber")
 
 # Define the window's contents
 layout = [
     [sg.Text("Look it up on Google:")],
     [
         sg.Input(
-            key="-INPUT-",
-            text_color="yellow",
-            font="Consolas 16",
+            key="-INPUT_GOOGLE-",
+            # text_color="yellow",
+            font="Consolas 15",
+            do_not_clear=False,
+        )
+    ],
+    [sg.Text("Look it up on Youtube:")],
+    [
+        sg.Input(
+            key="-INPUT_YOUTUBE-",
+            # text_color="red",
+            font="Consolas 15",
             do_not_clear=False,
         )
     ],
@@ -27,7 +36,7 @@ layout = [
 ]
 
 # Create the window
-window = sg.Window("Window Title", layout, icon="./google.ico")
+window = sg.Window("Google & YouTube Look-up-er", layout, icon="./google.ico")
 
 # Display and interact with the Window using an Event Loop
 while True:
@@ -37,9 +46,13 @@ while True:
     if event == sg.WINDOW_CLOSED or event == "Quit":
         break
     else:
-        look_up = values["-INPUT-"]
+        if values["-INPUT_GOOGLE-"]:
+            look_up = values["-INPUT_GOOGLE-"]
 
-        url = f"https://google.com/search?q={look_up}"
+            url = f"https://google.com/search?q={look_up}"
+        elif values["-INPUT_YOUTUBE-"]:
+            look_up = values["-INPUT_YOUTUBE-"]
+            url = f"https://www.youtube.com/results?search_query={look_up}"
         webbrowser.get(
             "C:/Program Files (x86)/Google/Chrome/Application/chrome.exe %s"
         ).open(url)
