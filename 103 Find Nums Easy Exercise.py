05d1c513efb19987b8361722012358584b845caa
GeroZayas <gerozayas@gmail.com>
2022-12-07 09:43:36 +0100

    Initial

---
+++ 103 Find Nums Easy Exercise.py
@@ -0,0 +1,18 @@
+# Python Programming Puzzles [100 exercises with solution]
+# https://www.w3resource.com/python-exercises/puzzles/index.php
+
+"""
+1. Write a Python program find a list of integers with exactly two occurrences of nineteen and at least three occurrences of five. Go to the editor
+Input:
+[19, 19, 15, 5, 3, 5, 5, 2]
+Output:
+True
+Input:
+[19, 15, 15, 5, 3, 3, 5, 2]
+Output:
+False
+Input:
+[19, 19, 5, 5, 5, 5, 5]
+Output:
+True
+"""
