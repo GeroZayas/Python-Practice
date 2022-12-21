602b0c7c7e9a236cba91d2240ef2423a4a8873b9
GeroZayas <gerozayas@gmail.com>
2022-12-21 22:40:53 +0100

    Update

--- Flask_Practice/views.py
+++ Flask_Practice/views.py
@@ -4,8 +4,10 @@ from flask import Blueprint, render_template
 
 views = Blueprint('views', __name__)
 
-@views.route('/views')
+@views.route('/')
 def home():
     return render_template("index.html")
 
-
+@views.route('/profile')
+def method_name():
+    pass
