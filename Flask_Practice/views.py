9add4ed9ea8bfa22f8726426e39614048c11eab5
GeroZayas <gerozayas@gmail.com>
2022-12-21 23:00:48 +0100

    Update

--- Flask_Practice/views.py
+++ Flask_Practice/views.py
@@ -1,5 +1,5 @@
 #Run pip install flask-blueprint
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, jsonify
 
 
 views = Blueprint('views', __name__)
@@ -9,5 +9,13 @@ def home():
     return render_template("index.html")
 
 @views.route('/profile')
-def method_name():
-    pass
+def profile():
+    args = request.args
+    name = args.get('name')
+    return render_template("index.html", name = name)
+
+
+@views.route('/json')
+def get_json():
+    return jsonify({'name':'Gero', 'coolness':10})
+
