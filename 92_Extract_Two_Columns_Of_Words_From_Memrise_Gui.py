d6bcc44992fbf3df397027d3fc03661445f717df
GeroZayas <gerozayas@gmail.com>
2022-12-29 14:46:59 +0100

    Update

--- 92_Extract_Two_Columns_Of_Words_From_Memrise_Gui.py
+++ 92_Extract_Two_Columns_Of_Words_From_Memrise_Gui.py
@@ -7,24 +7,33 @@ from Extract_Vocab_From_Memrise_Courses import extract_vocab
 # TODO: d) Add own icon
 # TODO: e)
 
+import os
+
+os.system("cls")
+
+FONT = "Consolas 16"
+
+sg.theme("Sandy Beach")
+
 
 # Define the window's contents
 layout = [
-    [sg.Input(key="-INPUT-", size=80)],
+    [sg.Input(key="-INPUT-", size=80, font=FONT)],
     [
         sg.Text(
             text="INSERT LINK and WAIT TILL WE GET ALL THE VOCABULARY!",
             size=(80, 1),
             key="-OUTPUT-",
+            font=FONT,
         )
     ],
-    [sg.Button("Ok"), sg.Button("Quit")],
+    [sg.Button("Ok", font=FONT), sg.Button("Quit", font=FONT)],
 ]
 
 # Create the window
 window = sg.Window("Get Vocabulary from Memrise Course", layout)
 
-# Display and interact with the Window using an Event Loop 
+# Display and interact with the Window using an Event Loop
 while True:
     event, values = window.read()
     # See if user wants to quit or window was closed
