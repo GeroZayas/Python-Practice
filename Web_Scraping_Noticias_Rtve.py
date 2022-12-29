56bccec148ab8356cfc9da906bc282a042ed42c0
GeroZayas <gerozayas@gmail.com>
2022-12-29 11:27:03 +0100

    Update names

---
+++ Web_Scraping_Noticias_Rtve.py
@@ -0,0 +1,29 @@
+def get_RTVE_news_to_txt_file():
+    from bs4 import BeautifulSoup
+    import requests
+    import csv
+
+    response = requests.get("https://www.rtve.es/noticias/")
+    noticias_rtve = response.text
+
+    soup = BeautifulSoup(noticias_rtve, "html.parser")
+
+    maintitle = soup.find_all(name="span", attrs={"class": "maintitle"})
+
+    # for title in maintitle:
+    #     if "Telediario" in title.text:
+    #         break
+    #     print(title.text + "\n\n")
+
+    with open("news_scraped/news_from_rtve.txt", "w") as file:
+        for title in maintitle:
+            # This IF takes care of the problem of having the same line repeated on the txt file
+            if "Telediario" in title.text:
+                break
+            file.write(title.get_text() + "\n\n")
+
+    print("DONE")
+
+
+if __name__ == "__main__":
+    get_RTVE_news_to_txt_file()
