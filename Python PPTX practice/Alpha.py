cdf5d29ae35127e752cd7c237bc22863288d8165
GeroZayas <gerozayas@gmail.com>
2023-02-19 19:39:45 +0100

    update

--- Python PPTX practice/Alpha.py
+++ Python PPTX practice/Alpha.py
@@ -8,12 +8,15 @@ from pptx import Presentation
 
 
 prs = Presentation()
-title_slide_layout = prs.slide_layouts[0]
+title_slide_layout = prs.slide_layouts[2]
 slide = prs.slides.add_slide(title_slide_layout)
 title = slide.shapes.title
 subtitle = slide.placeholders[1]
 
 title.text = "Hello, my name is Gero!"
 subtitle.text = "this is pretty cool!"
+
+for s in range(5):
+    prs.slides.add_slide(prs.slide_layouts[5])
 
 prs.save('Gero Zayas.pptx')
