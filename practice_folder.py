68b4fdc8950f7ea024d24dc5f41760cc53f78e13
GeroZayas <gerozayas@gmail.com>
2023-03-09 11:19:57 +0100

    Update

--- practice_folder.py
+++ practice_folder.py
@@ -1,5 +1,11 @@
+import pyperclip
+
 folder_path = (
     r"C:\Users\Gero Zayas\Downloads\CODING\0 GERO PYTHON\Git-Hub Python Practice"
 )
 
-print("chdir " + folder_path)
+print("This is copied to clipboard! >>> chdir " + folder_path)
+print("Just paste it!")
+
+pyperclip.copy("chdir " + folder_path)
+
