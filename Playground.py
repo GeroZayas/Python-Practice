57cea63cb554d7680f3f74cb765aa73e1030c6b9
GeroZayas <gerozayas@gmail.com>
2023-03-15 08:03:09 +0100

    Update

--- Playground.py
+++ Playground.py
@@ -3,7 +3,30 @@ class Person:
         self._name = name
         self._age = age
 
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def set_name(self, name):
+        self._name = name
+
+    @property
+    def age(self):
+        return self._age
+
+    @age.setter
+    def set_age(self, age):
+        self._age = age
+
+    def __str__(self):
+        return f"Name: {self._name}, Age: {self._age}"
+
 
 person1 = Person("John", 36)
 
-print()
+print(person1.name)
+
+person1.set_name = "Gero"
+
+print(person1.name)
