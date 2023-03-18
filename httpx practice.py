33d78e76203434ae286138bd11cfc22155c3b16f
GeroZayas <gerozayas@gmail.com>
2023-03-18 08:12:08 +0100

    Update

--- httpx practice.py
+++ httpx practice.py
@@ -1,25 +1,34 @@
 import httpx
 from bs4 import BeautifulSoup
 
+artist = None
+song = None
 
-artist = input("Artist: ")
-song = input("Song: ")
+
+# Asking the user to input the artist and song name.
+if not artist and not song:
+    artist = input("Artist: ").lower().strip().replace(" ", "")
+    song = input("Song: ").lower()
+
+# print("Artist", artist)
+# print("Song", song)
 
 params = {
     "artist": artist,
     "song": artist,
 }
 
+# Making a request to the website and getting the response.
 response = httpx.get(f"https://www.azlyrics.com/lyrics/{artist}/{song}.html")
 
 soup = BeautifulSoup(response.content, "html.parser")
+
 
 # Extract the title of the page
 title = soup.title.string
 print(title)
 
-print("Response's status code:\n->", response.status_code)
-
-print("Response's headers:\n->", response.headers)
-
-print("Response's text:\n->", response.text)
+print("-" * 60)
+content = soup.select_one(".col-xs-12 > div:nth-child(8)").text.strip()
+print(content)
+print("-" * 60)
