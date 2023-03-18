983a0c874c36961b04993d66c8d9eb09605fb978
GeroZayas <gerozayas@gmail.com>
2023-03-18 07:52:51 +0100

    UPdate

--- httpx practice.py
+++ httpx practice.py
@@ -1,9 +1,25 @@
 import httpx
+from bs4 import BeautifulSoup
 
-response = httpx.get("https://www.azlyrics.com/lyrics/loony/beg.html")
+
+artist = input("Artist: ")
+song = input("Song: ")
+
+params = {
+    "artist": artist,
+    "song": artist,
+}
+
+response = httpx.get(f"https://www.azlyrics.com/lyrics/{artist}/{song}.html")
+
+soup = BeautifulSoup(response.content, "html.parser")
+
+# Extract the title of the page
+title = soup.title.string
+print(title)
 
 print("Response's status code:\n->", response.status_code)
 
 print("Response's headers:\n->", response.headers)
 
-
+print("Response's text:\n->", response.text)
