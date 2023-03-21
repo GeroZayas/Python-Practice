f9765e9d31c377e38d49d1d41786ba567444abe0
GeroZayas <gerozayas@gmail.com>
2023-03-21 10:26:15 +0100

    Update

--- pf.py
+++ pf.py
@@ -1,11 +1,11 @@
 import pyperclip
+import subprocess
 
 folder_path = (
     r"C:\Users\Gero Zayas\Downloads\CODING\0 GERO PYTHON\Git-Hub Python Practice"
 )
 
-print("This is copied to clipboard! >>> chdir " + folder_path)
+print(f"This is copied to clipboard! >>> chdir {folder_path}")
 print("Just paste it!")
 
-pyperclip.copy("chdir " + folder_path)
-
+pyperclip.copy(f"chdir {folder_path}")
