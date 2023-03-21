a266b2bf649ae9769ac62a39921149c9c4370a6f
GeroZayas <gerozayas@gmail.com>
2023-03-21 14:58:42 +0100

    Update

--- Playground.py
+++ Playground.py
@@ -1,14 +1,9 @@
-file_name = "34_my_file.py"
-print("file_name: ", file_name)
+from random import sample, choice
 
+d = {"spam": 2, "eggs": 3, "green stuff": 4}
 
+random_value = sample(list(d.values()), 1)
+print("random_value: ", random_value)
 
-if file_name[0].isdigit():
-    for e in file_name:
-        if e.isdigit():
-            file_name = file_name.replace(e, "")
-print("file_name: ", file_name)
-
-if file_name.startswith("_"):
-    new_file_name = file_name[1:]
-    print("new_file_name : ", new_file_name)
+random_value = d[choice(list(d.keys()))]
+print("random_value: ", random_value)
