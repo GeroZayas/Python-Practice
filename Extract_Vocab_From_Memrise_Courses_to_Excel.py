3f1f555577e7d9bc853f37a79ffeb9fa7088b3f6
GeroZayas <gerozayas@gmail.com>
2023-03-21 08:55:39 +0100

    add comments

--- Extract_Vocab_From_Memrise_Courses_to_Excel.py
+++ Extract_Vocab_From_Memrise_Courses_to_Excel.py
@@ -17,39 +17,34 @@ def extract_vocab(link):
     WEB = link
 
     # ******* GET Number of Levels ********
+    # Getting the response from the website.
     response = requests.get(f"{WEB}")
 
+    # Getting the text from the response.
     data = response.text
 
+    # Parsing the data from the website.
     soup = BeautifulSoup(data, "html.parser")
 
     # Finding all the divs with the class level-index.
     level_number = soup.find_all(name="div", attrs={"class": "level-index"})
 
-    level_number_list = []
-
-    # Iterating over the list of levels and appending the text of each level to the list.
-    for level in level_number:
-        level_number_list.append(level.getText())
-
+    level_number_list = [level.getText() for level in level_number]
     LEVELS = int(level_number_list[-1])
-
-    # ********* END -> GET Number of Levels ******************
-
-    counter_level = 0
 
     CURRENT_PATH = os.getcwd()
 
     print(CURRENT_PATH)
 
-    for time in range(LEVELS):
-        counter_level += 1
+    progress_bar = "*"
+    col = 0
 
+    # A progress bar.
+    for counter_level, _ in enumerate(range(LEVELS), start=1):
         # ***** Progress Bar *****
 
         os.system("cls")
         extract_vocab.percentage = (counter_level / LEVELS) * 100
-        progress_bar = "*"
         print(
             progress_bar * counter_level
             + " " * (LEVELS - counter_level)
@@ -66,30 +61,27 @@ def extract_vocab(link):
 
         course_name = soup.find(name="h1", attrs={"class": "course-name"})
 
+        # Finding all the divs with the class col_a col text and col_b col text.
         first_language = soup.find_all(name="div", attrs={"class": "col_a col text"})
         second_language = soup.find_all(name="div", attrs={"class": "col_b col text"})
 
-        first_language_list = []
-        second_language_list = []
-
-        for word in first_language:
-            first_language_list.append(word.get_text())
-
-        for word in second_language:
-            second_language_list.append(word.get_text())
-
+        first_language_list = [word.get_text() for word in first_language]
+        second_language_list = [word.get_text() for word in second_language]
         # print(len(second_language_list))
 
         # how to put two lists into a dictionary
         words_dict = dict(zip(first_language_list, second_language_list))
 
+        # Replacing the "/" and ":" characters in the course name with "-" because the "/" and ":" characters
+        # are not allowed in file names.
         course_name_string = course_name.get_text().strip()
         if "/" in course_name_string:
             course_name_string = course_name_string.replace("/", "-")
         elif ":" in course_name_string:
             course_name_string = course_name_string.replace(":", "-")
 
-        if not os.path.exists(CURRENT_PATH + f"\{course_name_string}"):
+        # Checking if the folder with the course name exists. If it doesn't exist, it creates it.
+        if not os.path.exists(f"{CURRENT_PATH}\{course_name_string}"):
             os.makedirs(f"{course_name_string}")
 
         # Create a workbook and add a worksheet.
@@ -98,16 +90,10 @@ def extract_vocab(link):
         )
         worksheet = workbook.add_worksheet()
 
-        # Start from the first cell. Rows and columns are zero indexed.
-        row = 0
-        col = 0
-
         # Iterate over the data and write it out row by row.
-        for item, cost in words_dict.items():
+        for row, (item, cost) in enumerate(words_dict.items()):
             worksheet.write(row, col, item)
             worksheet.write(row, col + 1, cost)
-            row += 1
-
         workbook.close()
 
     print("DONE")
