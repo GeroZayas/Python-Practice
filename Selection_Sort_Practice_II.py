377791cb21d910ecca9334e3f529b613613114b3
GeroZayas <gerozayas@gmail.com>
2023-04-04 16:11:32 +0200

    Update

--- Selection_Sort_Practice_II.py
+++ Selection_Sort_Practice_II.py
@@ -2,22 +2,29 @@
 
 from random import randint
 
-numbers = [randint(1,8) for _ in range(4)]
+numbers = [randint(1,1000) for _ in range(10)]
 
 print(numbers)
-print(len(numbers))
+print("This is length of numbers =>", len(numbers))
 
-def selection_sort(arr:list)->None:
+def selection_sort(arr:list[int])->None:
     if len(arr) <= 1:
         return arr
-    for i in range(len(arr) - 1):
-        smallest_num_index = i
-        for j in range(smallest_num_index + 1, len(arr)):
-            if arr[j] < arr[smallest_num_index]:
-                smallest_num_index = j
-        arr[i], arr[smallest_num_index] = arr[smallest_num_index], arr[i]
-        
+    for i in range(len(arr)-1):
+        smallest_elem_idx = i
+        for j in range(smallest_elem_idx + 1, len(arr)):
+            if arr[j] < arr[smallest_elem_idx]:
+                smallest_elem_idx = j
+        arr[i], arr[smallest_elem_idx] \
+        = arr[smallest_elem_idx], arr[i]
+
+
+
 selection_sort(numbers)
 
 print("Sorted numbers:", numbers)
+
+
+
+
 
---
+++ testing_subprocess.py
@@ -0,0 +1,8 @@
+import os
+
+# folder_path = 'C:/Users/Gero Zayas/Downloads/CODING/0 GERO PYTHON/Git-Hub Python Practice'
+folder_path = ("C:/")
+
+os.chdir(folder_path)
+
+
