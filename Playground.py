062e09df7d723f9caf0bfaa01c8ea8ddfe0429d2
Gero Zayas <gerozayas@gmail.com>
2023-04-17 23:29:33 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -1,17 +1,30 @@
-numbers = [7, 6, 5, 4, 3, 2, 1]
 
-print("numbers: ", numbers)
+def findPairs(nums, target):
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if nums[i] + nums[j] == target:
+                return (nums[i], nums[j])
+    return ()
+
+arr = [8, 7, 2, 5, 3, 1]
+target = 10
+
+r = findPairs(arr, target)
+print(r)
 
 
+arr = [5, 2, 6, 8, 1, 9]
+target = 12
+
+r = findPairs(arr, target)
+print(r)
 
 
+arr = [1,1,1,1]
+target = 2
 
-
-
-
-
-
-
+r = findPairs(arr, target)
+print(r)
 
 
 
