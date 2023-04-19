3927c0330701c3f45d6b291643f53e389f9c0fda
Gero Zayas <gerozayas@gmail.com>
2023-04-19 21:47:53 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -2,9 +2,7 @@ def has_zero_sum_list(nums):
     s = {0} # we create a set and we add zero to it
     total = 0
     for i in nums:
-        print(f"Total {total} + {i} (i)")
         total += i
-        print(f"Set {s}", f"Total {total}")
         if total in s:
             return True
         s.add(total)
@@ -13,7 +11,7 @@ def has_zero_sum_list(nums):
 
 if __name__ == '__main__':
  
-    nums = [0, 4, -6, 3, -1, 4, 2, 7]
+    nums = [4, -6, 3, -1, 4, 2, 7]
  
     if has_zero_sum_list(nums):
         print('Sublist exists')
