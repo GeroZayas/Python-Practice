df04166a5e10fd760f76a5216551c12740289bc2
Gero Zayas <gerozayas@gmail.com>
2023-04-19 21:59:42 +0200

    Initial

---
+++ all_sublists_zero.py
@@ -0,0 +1,11 @@
+def print_all_sublists(nums):
+    for i in range(len(nums)):
+        total = 0
+        for j in range(i, len(nums)):
+            total += nums[j]
+            if total == 0:
+                print("Sublist",(nums[i:j+1]))
+                
+if __name__ == "__main__":
+    nums = [3, 4, -7, 3, 1, 3, 1, -4, -2, -2]
+    print_all_sublists(nums)
