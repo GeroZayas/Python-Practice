ae2d51d46ac330afa13d5bd5e63b6271d1acc58d
Gero Zayas <gerozayas@gmail.com>
2023-04-22 12:12:40 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -2,22 +2,17 @@ arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
 
 print("This is arr => ",arr)
 
-def selection_sort(arr):
+def quicksort(arr):
     size = len(arr)
     if size <= 1:
         return arr
-    
-    min_idx = 0
-
-    for i in range(size):
-        min_idx = i
-        for j in range(i+1, size):
-            if arr[j] < arr[min_idx]:
-                min_idx = j
-        arr[i], arr[min_idx] = arr[min_idx], arr[i]
-
-selection_sort(arr)
-
-print('This is sorted arr => ', arr)
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x <= pivot]
+    right = [x for x in arr[1:] if x > pivot]
+    return quicksort(left) + [pivot] + quicksort(right)
+                
+        
+        
+print('This is sorted arr => ', quicksort(arr))
 
 
