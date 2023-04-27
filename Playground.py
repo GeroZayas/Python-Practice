90b1c69c1d53b84cede70766f2d78a726e960d5f
Gero Zayas <gerozayas@gmail.com>
2023-04-27 08:07:02 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -4,15 +4,15 @@ class Human:
         self.age = age
         self.job = job
 
-    def show_name():
+    def show_name(self):
         print(self.name)
         return self
     
-    def show_age():
+    def show_age(self):
         print(self.age)
         return self
 
 
 gero = Human('Gero', 31, 'Programmer')
 
-print(gero.show_name.show_age)
+print(gero.show_name().show_age().show_name())
