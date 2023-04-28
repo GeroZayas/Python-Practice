25a06fe541420724e6802a8614945d996ae3e5cd
Gero Zayas <gerozayas@gmail.com>
2023-04-28 08:08:55 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -1,3 +1,18 @@
-text = "the quick brown fox jumped over the the quick brown quick log log"
-heap = list(set(text.split()))
-print(heap)
+# binary search tree 
+def  binary_search(arr, item):
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        mid = (low + high) // 2
+        guess = arr[mid]
+        if guess == item:
+            return mid
+        if guess > item:
+            high = mid - 1
+        else:
+            low = mid + 1
+    return None
+
+arr = [1,2,3,4,5,6,7,8,9,10]
+
+print(binary_search(arr,8))
