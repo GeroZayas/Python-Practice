a43c54b7cb06f94d69bcc30a5a5b1ceab90a8dc4
Gero Zayas <gerozayas@gmail.com>
2023-04-28 07:56:51 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -1,18 +1,3 @@
-class Human:
-    """A class for a Human"""
-    def __init__(self, name, age, job):
-        self.name = name
-        self.age = age
-        self.job = job
-
-    def show_name(self):
-        print(self.name)
-        return self
-    
-    def show_age(self):
-        print(self.age)
-        return self
-
-
-Human.alive = "I am alive"
-Human.place = 'Earth'
+text = "the quick brown fox jumped over the the quick brown quick log log"
+heap = list(set(text.split()))
+print(heap)
