7a8dc802f425abb9b98874f4bc7b25b8bb580d32
Gero Zayas <gerozayas@gmail.com>
2023-04-30 08:39:41 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -7,3 +7,25 @@ def quicksort(arr):
     less = [i for i in arr[1:] if i <= pivot]
     greater = [i for i in arr[1:] if i > pivot]
     return quicksort(less) + [pivot] + quicksort(greater)
+
+def merge_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    mid = len(arr) // 2
+    left = merge_sort(arr[:mid])
+    right = merge_sort(arr[mid:])
+    return merge(left, right)
+    
+def merge(left, right):
+    result = []
+    i, j = 0, 0
+    while i < len(left) and j < len(right):
+        if left[i] <= right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
