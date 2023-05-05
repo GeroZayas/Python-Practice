926ea91f4b0d73862c5b258d461f1461874044d4
Gero Zayas <gerozayas@gmail.com>
2023-05-06 00:05:36 +0200

    update

--- Playground.py
+++ Playground.py
@@ -1,49 +1,6 @@
-from random import randint, choice
+import heapq
 
-numbers = [randint(1,30) for _ in range(9)]
-
-print('numbers: ', numbers)
-
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]
-    left = [x for x in arr[1:] if x < pivot]
-    right = [x for x in arr[1:] if x >= pivot]
-    
-    return quicksort(left) + [pivot] + quicksort(right)
-
-sorted_numbers = quicksort(numbers)
-
-print('sorted_numbers : ', sorted_numbers )
-
-##########################################################
-print("-" * 60)
-##########################################################
-
-def binary_search(arr, target):
-    left, right = 0, len(arr) - 1
-    while left < right:
-        mid = (left + right) // 2
-        
-        if target == arr[mid]:
-            return f"Element {target} found at index {mid}"
-        elif target < arr[mid]:
-            return binary_search(arr[:mid-1], target)
-        else:
-            return binary_search(arr[mid+1:], target)
-        
-    return "Element not found in array"
-
-target = choice(sorted_numbers)
-print(target)
-
-r = binary_search(sorted_numbers, target)
-
-print(r)
-
-
-
-
+H = [3,7,6,3,9,2,5]
+print('H: ', H)
 
 
