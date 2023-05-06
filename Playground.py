48c857b1def2f8168828825fcb348e09848eeff4
Gero Zayas <gerozayas@gmail.com>
2023-05-06 08:39:36 +0200

    add Binary Search

--- Playground.py
+++ Playground.py
@@ -19,12 +19,21 @@ print('sorted_numbers: ', sorted_numbers)
 
 
 # Binary Search
-
+def binary_search(arr, target):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            return f"Element {target} found at index {mid}"
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return "Element not found in arr" 
 
 target = choice(sorted_numbers)
 print('target : ', target )
-
-print('type(choice(sorted_numbers)): ', type(choice(sorted_numbers)))
 
 r = binary_search(sorted_numbers, target)
 print('r = binary_search: ', r)
