f656b89fafd4d0727bf44e0bb2fe422178587d5d
Gero Zayas <gerozayas@gmail.com>
2023-05-06 16:02:54 +0200

    Update

--- group_anagrams_leetcode_exercise.py
+++ group_anagrams_leetcode_exercise.py
@@ -13,20 +13,19 @@ def groupAnagrams(strs: typing.List[str]) -> typing.List[typing.List[str]]:
 
     for s in strs:
         count = [0] * 26
-        print("-" * 60) ####################
         
         for c in s:
             count[ord(c) - ord("a")] += 1
-            print('count: ', count)
-            print("-" * 60) ####################
             
         ans[tuple(count)].append(s)
-        print('ans: ', ans)
-        print("-" * 60) ####################
         
-    return ans.values()
+    return ans.values() # type: ignore
 
 
 strs = ["eat","tea","tan","ate","nat","bat"]
+
+print(groupAnagrams(strs=strs))
+
+strs = ['a', 'b', 'b']
 
 print(groupAnagrams(strs=strs))
