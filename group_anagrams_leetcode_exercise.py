4fe7678186f7b1dd430cf2833956e8ffddcb5294
Gero Zayas <gerozayas@gmail.com>
2023-05-06 16:44:58 +0200

    Update

--- group_anagrams_leetcode_exercise.py
+++ group_anagrams_leetcode_exercise.py
@@ -1,31 +1,25 @@
-import typing, collections
+from typing import List
+from collections import defaultdict
 
-def groupAnagrams(strs: typing.List[str]) -> typing.List[typing.List[str]]:
-    """
-    The function takes a list of strings and groups them into lists of anagrams using a dictionary with
-    character count tuples as keys.
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+    ans = defaultdict(list)
     
-    :param strs: A list of strings that we want to group into anagrams
-    :type strs: typing.List[str]
-    :return: a list of lists of strings, where each inner list contains a group of anagrams.
-    """
-    ans = collections.defaultdict(list)
-
     for s in strs:
         count = [0] * 26
-        
         for c in s:
-            count[ord(c) - ord("a")] += 1
-            
+            count[ord(c) - ord('a')] += 1
         ans[tuple(count)].append(s)
         
-    return ans.values() # type: ignore
-
+    return ans.values()
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 
 print(groupAnagrams(strs=strs))
 
 strs = ['a', 'b', 'b']
+
+print(groupAnagrams(strs=strs))
+
+strs = ['']
 
 print(groupAnagrams(strs=strs))
