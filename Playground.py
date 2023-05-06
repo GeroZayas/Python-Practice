3ad3065bf2d39cb010301535f2e3cf1aabfe6a4e
Gero Zayas <gerozayas@gmail.com>
2023-05-06 10:08:02 +0200

    Add Sliding Window funct

--- Playground.py
+++ Playground.py
@@ -1,39 +1,15 @@
-from random import choice
-
-numbers = [7,3,2,54,8,24,85,23]
-print('numbers: ', numbers)
-
-# Quicksort
-def quicksort(arr):
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]
+def sliding_window(arr, k):
+    # k => the size of the window
+    size = len(arr)
+    if k > size:
+        return None
+    window_sum = sum(arr[:k])
+    max_sum = window_sum
+    for i in range(size-k):
     
-    left = [x for x in arr[1:] if x < pivot]
-    right = [x for x in arr[1:] if x >= pivot]
     
-    return quicksort(left) + [pivot] + quicksort(right)
-
-sorted_numbers = quicksort(numbers)
-print('sorted_numbers: ', sorted_numbers)
-
-
-# Binary Search
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if target == arr[mid]:
-            return f"Element {target} found at index {mid}"
-        elif target < arr[mid]:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return "Element not found in arr" 
-
-target = choice(sorted_numbers)
-print('target : ', target )
-
-r = binary_search(sorted_numbers, target)
-print('r = binary_search: ', r)
+"""
+size = 6
+k = 2
+[3,4,6,7,8,2]
+"""
