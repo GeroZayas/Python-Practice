8517c162c0048af342e4983e07261088701e8c7e
Gero Zayas <gerozayas@gmail.com>
2023-05-06 08:36:52 +0200

    Fix algo

--- Playground.py
+++ Playground.py
@@ -19,18 +19,7 @@ print('sorted_numbers: ', sorted_numbers)
 
 
 # Binary Search
-def binary_search(arr, target):
-    left = 0
-    right = len(arr) - 1
-    while left <= right:
-        mid = (left + right) // 2
-        if target == arr[mid]:
-            return f"Element found at index {mid}"
-        elif target < arr[mid]:
-            right = mid - 1
-        else:
-            left = mid + 1
-    return 'Element not found'
+
 
 target = choice(sorted_numbers)
 print('target : ', target )
