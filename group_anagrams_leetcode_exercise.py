5b0dd69f66d6f0f706bae91527f4c60bd0faa055
Gero Zayas <gerozayas@gmail.com>
2023-05-07 12:21:04 +0200

    Update funct

--- group_anagrams_leetcode_exercise.py
+++ group_anagrams_leetcode_exercise.py
@@ -2,7 +2,12 @@ from typing import List
 from collections import defaultdict
 
 def groupAnagrams(strs: List[str]) -> List[List[str]]:
-   
+    ans = defaultdict(list)
+    for s in strs:
+        count = [0] * 26
+        for c in s:
+            count[ord(c) - ord('a')] += 1
+        ans[count].append(s)
 
 strs = ["eat","tea","tan","ate","nat","bat"]
 
