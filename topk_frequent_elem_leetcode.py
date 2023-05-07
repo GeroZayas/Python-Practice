13652d277e0742ad2f33fa9338859e55c229b01b
Gero Zayas <gerozayas@gmail.com>
2023-05-07 20:24:19 +0200

    Update

--- topk_frequent_elem_leetcode.py
+++ topk_frequent_elem_leetcode.py
@@ -1,45 +1,46 @@
-from typing import List
+# from typing import List
 
-'''
-Given an integer array nums and an integer k, 
-return the k most frequent elements. 
-You may return the answer in any order.
+# '''
+# Given an integer array nums and an integer k, 
+# return the k most frequent elements. 
+# You may return the answer in any order.
 
-Example 1:
+# Example 1:
 
-Input: nums = [1,1,1,2,2,3], k = 2
-Output: [1,2]
+# Input: nums = [1,1,1,2,2,3], k = 2
+# Output: [1,2]
 
-Example 2:
+# Example 2:
 
-Input: nums = [1], k = 1
-Output: [1]
-'''
+# Input: nums = [1], k = 1
+# Output: [1]
+# '''
 
-def topKFrequent(nums: List[int], k: int) -> List[int]:
-    numbers = {n:0 for n in set(nums)}
-    print(numbers)
-    for num in nums:
-        numbers[num]+= 1
+# def topKFrequent(nums: List[int], k: int) -> List[int]:
+#     numbers = {n:0 for n in set(nums)}
+#     print(numbers)
+#     for num in nums:
+#         numbers[num]+= 1
         
-    freq = {}
-    for key, value in numbers.items():
-        if value in freq:
-            freq[value].append(key)
-        else:
-            freq[value] = [key]
+#     freq = {}
+#     for key, value in numbers.items():
+#         if value in freq:
+#             freq[value].append(key)
+#         else:
+#             freq[value] = [key]
 
-    result = []
-    for count in sorted(freq, reverse=True):
-        result.extend(freq[count])
-        if len(result) >= k:
-            break
+#     result = []
+#     for count in sorted(freq, reverse=True):
+#         result.extend(freq[count])
+#         if len(result) >= k:
+#             break
 
-    return result # type: ignore
+#     return result # type: ignore
 
-nums = [1,1,1,1,1,1,2,2,3,3,3,3,3,3,3,3,3,3,3]
-k = 2
+# nums = [1,1,1,1,1,1,2,2,3,3,3,3,3,3,3,3,3,3,3]
+# k = 2
 
-r = topKFrequent(nums, k)
+# r = topKFrequent(nums, k)
 
-print('r = topKFrequent(nums, k): ', r)
+# print('r = topKFrequent(nums, k): ', r)
+
