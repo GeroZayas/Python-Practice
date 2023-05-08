490837e8165f339100b8b73635e23bc1d68b6567
Gero Zayas <gerozayas@gmail.com>
2023-05-08 10:08:04 +0200

    Update

--- Product of Array Except Self leetcode.py
+++ Product of Array Except Self leetcode.py
@@ -1,3 +1,5 @@
+from typing import List
+
 '''
 Given an integer array nums, return an array answer such that answer[i] 
 is equal to the product of all the elements of nums except nums[i].
@@ -19,4 +21,24 @@ Input: nums = [-1,1,0,-3,3]
 Output: [0,0,9,0,0]
 '''
 
-def productExceptSelf(self, nums: List[int]) -> List[int]:
+def productExceptSelf(nums: List[int]) -> List[int]:
+    res = [1] * len(nums)
+    
+    prefix = 1
+    for i in range(len(nums)):
+        res[i] = prefix
+        prefix *= nums[i]
+    postfix = 1
+    for i in range(len(nums) - 1, -1, -1):
+        res[i] *= postfix
+        postfix *= nums[i]
+    
+    return res
+
+nums = [1,2,3,4]
+
+print(productExceptSelf(nums))
+
+nums = [-1,1,0,-3,3]
+
+print(productExceptSelf(nums))
