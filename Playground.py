ebd201fbe28c30da02360603f05b6a7922417548
Gero Zayas <gerozayas@gmail.com>
2023-05-08 07:52:39 +0200

    Update funct

--- Playground.py
+++ Playground.py
@@ -1,23 +1,7 @@
 nums = [1,1,1,2,2,3,4,4,4,4,4,4]; k = 2
 
 def topK_frequent(nums, k):
-    count = {} 
-    
-    freq = [ [] for _ in range(len(nums) + 1)]
-    
-    for n in nums:
-        count[n] = 1 + count.get(n, 0)
-    
-    for n, c in count.items():
-        freq[c].append(n)
-    
-    res = []
-    for i in range(len(freq) - 1, 0, -1):
-        for n in freq[i]:
-            res.append(n)
-            if len(res) == k:
-                return res
-    
+   
 
 
 r = topK_frequent(nums, k)
