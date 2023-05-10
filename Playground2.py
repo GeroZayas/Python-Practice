8038bbce118e54ff1aada4c1fabdf84a378ab988
Gero Zayas <gerozayas@gmail.com>
2023-05-10 08:18:20 +0200

    Unpdate funct

--- Playground2.py
+++ Playground2.py
@@ -1,4 +1,4 @@
-
+from random import randint
 
 def quicksort(arr):
     if len(arr) <= 1:
@@ -8,4 +8,15 @@ def quicksort(arr):
     right = [e for e in arr[1:] if e >= pivot]
     
     return quicksort(left) + [pivot] + quicksort(right)
+
+numbers = [randint(1,20) for _ in range(10)]
+print('numbers: ', numbers)
+
+# ----------------------------------------------------------------
+
+print("-" * 60) # CONSOLE SEP
+
+res = quicksort(numbers)
+print('res: ', res)
+
 
