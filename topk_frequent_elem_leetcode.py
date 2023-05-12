19883dac44fa61bb999734a2e6a3e13e53ab2c00
Gero Zayas <gerozayas@gmail.com>
2023-05-12 10:28:59 +0200

    Update

--- topk_frequent_elem_leetcode.py
+++ topk_frequent_elem_leetcode.py
@@ -45,25 +45,7 @@
 # print('r = topKFrequent(nums, k): ', r)
 
 def topK_frequent(nums:list, k:int)->list[int]:
-    count = {}
-    freq = [[] for _ in range(len(nums) + 1)]
     
-    for n in nums:
-        count[n] = 1 + count.get(n, 0)
-        
-    for n, c in count.items():
-        freq[c].append(n)
-        
-    print(count)
-    print(freq)
-
-    res = []
-    
-    for i in range(len(freq) - 1, 0, -1):
-        for n in freq[i]:
-            res.append(n)
-            if len(res) == k:
-                return res
     
 nums = [1,1,1,2,2,3,4,4,4,4,4,4]; k = 2
 
