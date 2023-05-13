27d0ca32cd404f692b269538e3a1fe1b2a0df1aa
Gero Zayas <gerozayas@gmail.com>
2023-05-13 09:10:25 +0200

    add quicksort

--- Playground2.py
+++ Playground2.py
@@ -1,6 +1,11 @@
 from random import randint
+from typing import List
 
 numbers = [randint(1,100) for _ in range(10)]
 
 print('numbers: ', numbers)
 
+# ----------------------------------------------------------------
+
+def quicksort(arr:List[int])-> List[int]:
+    
