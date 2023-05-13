bcf5233c14a3b9b2a596d44c0bebad90b1c6d360
Gero Zayas <gerozayas@gmail.com>
2023-05-13 09:14:38 +0200

    add Binary search

--- Playground2.py
+++ Playground2.py
@@ -8,4 +8,31 @@ print('numbers: ', numbers)
 # ----------------------------------------------------------------
 
 def quicksort(arr:List[int])-> List[int]:
+    if len(arr) <= 1:
+        return arr
+    pivot = arr[0]
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
     
+    return quicksort(left) + [pivot] + quicksort(right)
+
+sorted_nums = quicksort(numbers)
+
+print('sorted_nums : ', sorted_nums )
+
+# ----------------------------------------------------------------
+
+def binary_search(arr: List[int], target:int):
+    left = 0
+    right = len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == arr[mid]:
+            return f"Element {target} found at index {mid}"
+        elif target < arr[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+            
+    return "Element not found in array"
+
