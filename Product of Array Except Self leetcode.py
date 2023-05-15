4f61e90c0b308f0bcfcedb9a7f52e4a47d929f66
Gero Zayas <gerozayas@gmail.com>
2023-05-15 09:18:04 +0200

    Update

--- Product of Array Except Self leetcode.py
+++ Product of Array Except Self leetcode.py
@@ -22,7 +22,7 @@ Output: [0,0,9,0,0]
 '''
 
 def productExceptSelf(nums: List[int]) -> List[int]:
-    res = [1] * len(nums)
+    res = [0] * len(nums)
     
     prefix = 1
     for i in range(len(nums)):
@@ -31,12 +31,10 @@ def productExceptSelf(nums: List[int]) -> List[int]:
     
     postfix = 1
     for i in range(len(nums) - 1, -1, -1):
-        res[i] *= postfix
-        postfix *= nums[i]
+        res[i] *= postfix 
+        postfix *= nums[i]   
     
-        
     return res
-    
     
     
 nums = [1,2,3,4]
