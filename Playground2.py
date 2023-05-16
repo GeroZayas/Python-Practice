916044c863e950b536b20c58f92b20b4b79f9b77
Gero Zayas <gerozayas@gmail.com>
2023-05-16 22:43:30 +0200

    Update

--- Playground2.py
+++ Playground2.py
@@ -1,11 +1,17 @@
+from collections import defaultdict
 from typing import List
 
 
-def groupAnagrams(strs: List[str]) -> List[List[str]]:
-        dic = {}
+def groupAnagrams(strs: List[str]) -> List[List[str]]: # type: ignore
+        dic = defaultdict(list)
 
         for s in strs:
             word_list = [0] * 26
             for c in s:
-                dic[word_list] = 1 + word_list.get(c, 0)
+                dic[word_list] = 1 + 
         print(dic)
+        
+        
+strs = ["eat","tea","tan","ate","nat","bat"]
+
+print(groupAnagrams(strs=strs))
