65046ed81ce4fed74f634cacfd5595bfc9031e39
Gero Zayas <gerozayas@gmail.com>
2023-05-16 22:41:00 +0200

    Update

--- Playground2.py
+++ Playground2.py
@@ -1,3 +1,11 @@
-nums = [1,2,3,4,5]
-for e in nums[::-1]:
-    print(e)
+from typing import List
+
+
+def groupAnagrams(strs: List[str]) -> List[List[str]]:
+        dic = {}
+
+        for s in strs:
+            word_list = [0] * 26
+            for c in s:
+                dic[word_list] = 1 + word_list.get(c, 0)
+        print(dic)
