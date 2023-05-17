32a2f1e0db7dcbdf8b3d8b8a5f52f2decdd61a8b
Gero Zayas <gerozayas@gmail.com>
2023-05-17 08:00:02 +0200

    Update

--- Playground2.py
+++ Playground2.py
@@ -3,14 +3,7 @@ from typing import List
 
 
 def groupAnagrams(strs: List[str]) -> List[List[str]]: # type: ignore
-        dic = defaultdict(list)
-
-        for s in strs:
-            word_list = [0] * 26
-            for c in s:
-                dic[word_list] = 1 + 
-        print(dic)
-        
+    
         
 strs = ["eat","tea","tan","ate","nat","bat"]
 
