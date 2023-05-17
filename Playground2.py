ce6b82660b1671873bf8c472789888638e0a463b
Gero Zayas <gerozayas@gmail.com>
2023-05-17 08:11:30 +0200

    Update

--- Playground2.py
+++ Playground2.py
@@ -4,7 +4,11 @@ from typing import List
 
 def groupAnagrams(strs: List[str]) -> List[List[str]]: # type: ignore
     res = defaultdict(list)
-    
+    the_words = strs
+    for word in the_words:
+        count = [0] * 26
+        for character in word:
+            count[ord]
         
 strs = ["eat","tea","tan","ate","nat","bat"]
 
