c378c654bb8cce0d8c64dc567962843a06274175
Gero Zayas <gerozayas@gmail.com>
2023-05-18 08:16:35 +0200

    Add solution

--- valid_sudoku_leetcode.py
+++ valid_sudoku_leetcode.py
@@ -1,8 +1,26 @@
 
 from typing import List
+from collections import defaultdict
 
 def isValidSudoku(board: List[List[str]]) -> bool:
-	...
+	rows = defaultdict(set)
+	colums = defaultdict(set)
+	squares = defaultdict(set)
+
+	for r in range(9):
+		for c in range(9):
+			if board[r][c] == '.':
+				continue
+			if (board[r][c] in rows[r] or
+				board[r][c] in colums[c] or
+				board[r][c] in squares[(r//3, c//3)]):
+				return False
+			colums[c].add(board[r][c])
+			rows[r].add(board[r][c])
+			squares[(r//3, c//3)].add(board[r][c])
+
+	return True
+
 
 board = [["5","3",".",".","7",".",".",".","."]
 ,["6",".",".","1","9","5",".",".","."]
