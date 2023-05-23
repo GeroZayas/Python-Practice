c5183741cd7b445c86811dfc576f6cc0d131b6ae
GeroZayas <gerozayas@gmail.com>
2023-05-23 11:39:17 +0200

    Complete binary Serach

--- Algorithms Gero/Binary_Search.py
+++ Algorithms Gero/Binary_Search.py
@@ -23,12 +23,22 @@ print("sorted_numbers: ", sorted_numbers)
 # IMPLEMENT Binary Search (recursively) to find a target num
 
 
-def binary_search(nums, left, right, target):
-    ...
+def binary_search(nums, target):
+    left, right = 0, len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return f"Element {target} found at index {mid}"
+        elif target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return f"Element {target} not found in array"
+    
 
 
-target, left, right = 80, 0, len(sorted_numbers) - 1
+target = 68
 
-r = binary_search(nums=sorted_numbers, left=left, right=right, target=target)
+r = binary_search(nums=sorted_numbers, target=target)
 
 print("binary_search: ", r)
