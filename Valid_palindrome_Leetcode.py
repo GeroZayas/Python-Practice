01c6b8e8534d9a01e064fb810a03064c70631aba
GeroZayas <gerozayas@gmail.com>
2023-05-24 08:38:48 +0200

    Add Solution 1

--- Valid_palindrome_Leetcode.py
+++ Valid_palindrome_Leetcode.py
@@ -7,3 +7,22 @@ Output: true
 Explanation: "amanaplanacanalpanama" is a palindrome.
 '''
 
+# SOLUTION 1
+'''
+Creates extra memory by creating a new string and also a reversed one at the end of the function
+'''
+
+def isPalindrome1(s: str) -> bool:
+	new_string = ''
+	for c in s:
+		if c.isalnum():
+			new_string += c.lower()
+	return new_string == new_string[::-1]
+
+print(isPalindrome1(s = "A man, a plan, a canal: Panama"))
+
+
+# SOLUTION 2
+
+def isPalindrome2(s: str) -> bool:
+	...
