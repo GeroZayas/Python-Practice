0a3b3c6f34a63a42cee60b238d44be3f1729be8f
GeroZayas <gerozayas@gmail.com>
2023-05-26 10:44:42 +0200

    add funct

--- Playground2.py
+++ Playground2.py
@@ -1,8 +1,16 @@
-def isPalindrome(string:str)->bool:
+def isPalindrome(string: str) -> bool:
+    def isalphanum(c: str) -> bool:
+        if (
+            ord("A") <= ord(c) <= ord("Z")
+            or ord("a") <= ord(c) <= ord("z")
+            or ord("0") <= ord(c) <= ord("9")
+        ):
+            return True
+        return False
+    
     
 
-
-
+print(isPalindrome("4"))
 
 
 # print("Solution 2: ->  ", isPalindrome(s = "A man, a plan, a canal: Panama"))
