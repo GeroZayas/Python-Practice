79e193ef8077c5129b8b8f2b64c7609857c61cbd
gero zayas <gerozayas@gmail.com>
2023-06-10 14:20:32 +0200

    Update

---
+++ PyData2023/machine_learning.py
@@ -0,0 +1,15 @@
+# PyData2024 June 10, Canodrom Ateneu Machine Learning Intro Talk
+""" Linear Regression -> foundational algorithms
+
+Deep learning is inside Machine Learning:
+Deep learning -> multiple blocks, several algorithms put together
+
+Common uses of Machine Learning:
+
+- Computer Vision
+- Recommendation Engines (Netflix)
+- Natural language Processing
+- Time Series (stock predictions, sales, etc)
+- Anomaly detections (e.g., someone steals your credit card)
+
+"""
