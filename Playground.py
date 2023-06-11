7d9bfe2c6ceafa3a1f4caa66a1dbe5fda587d460
gero zayas <gerozayas@gmail.com>
2023-06-11 20:26:25 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -2,3 +2,15 @@ def quicksort(arr:list)->list:
     if len(arr) <= 1:
         return arr
     pivot = arr[0]
+    left = [x for x in arr[1:] if x < pivot]
+    right = [x for x in arr[1:] if x >= pivot]
+    return quicksort(left) + [pivot] + quicksort(right)
+
+from random import randint
+
+numbers = [randint(1,20) for _ in range(10)]
+
+print(numbers)
+
+print(quicksort(numbers))
+    
