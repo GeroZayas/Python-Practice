8f93fca6fbd6fd5d6f50923d9f58be030e08a2f8
gero zayas <gerozayas@gmail.com>
2023-06-11 20:16:48 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -1,8 +1,4 @@
-def quicksort(arr):
+def quicksort(arr:list)->list:
     if len(arr) <= 1:
         return arr
     pivot = arr[0]
-    left = [x for x in arr[1:] if x < pivot]
-    right = [x for x in arr[1:] if x >= pivot]
-    return quicksort(left) + [pivot] + quicksort(right)
-
