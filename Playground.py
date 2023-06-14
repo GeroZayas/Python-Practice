4ae55291bc4e6ff51f55bbbff4dce38d71fcce7b
gero zayas <gerozayas@gmail.com>
2023-06-14 08:36:23 +0200

    Update

--- Playground.py
+++ Playground.py
@@ -1,19 +1,6 @@
-def quicksort(arr:list)->list:
-    if len(arr) <= 1:
-        return arr
-    pivot = arr[0]
-    left = [x for x in arr[1:] if x < pivot]
-    right = [x for x in arr[1:] if x >= pivot]
-    return quicksort(left) + [pivot] + quicksort(right)
-    pivot 
+import pandas as pd
 
-from random import randint
+df = pd.read_csv("./Notebooks/2020_renda_neta_mitjana_per_llar.csv")
 
-numbers = [randint(1,20) for _ in range(10)]
-
-print(numbers)
-
-print(quicksort(numbers))
-
-
+print(df)
 
