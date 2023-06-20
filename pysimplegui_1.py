83b71a2b9d6fb09b6ea2235b4860e1122f2bf010
gero zayas <gerozayas@gmail.com>
2023-06-20 16:22:41 +0200

    Update

--- pysimplegui_1.py
+++ pysimplegui_1.py
@@ -1,14 +1,16 @@
 import PySimpleGUI as sg
 
 layout = [
-    [sg.Text("Learn English Now!")],
-    [sg.Button('GO!')]
+    [sg.Text("Learn English Now!", background_color="darkred", font=("Arial", 30))],
+    [sg.Button(button_text="GO!", mouseover_colors="blue", button_color="darkgreen", font=("Arial", 14))],
 ]
 
-window = sg.Window("Learn English Now!", layout=layout)
+window = sg.Window(
+    "Learn English Now!", layout=layout, size=(400, 200), background_color="gold"
+)
 
 while True:
-    event, values = window.read() # type: ignore
+    event, values = window.read()  # type: ignore
     if event in [sg.WINDOW_CLOSED, "GO!"]:
         break
 
