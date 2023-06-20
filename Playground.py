f0f46a3fe0d254173902eb9ed87bea4d46db9625
gero zayas <gerozayas@gmail.com>
2023-06-20 08:04:52 +0200

    update

--- Playground.py
+++ Playground.py
@@ -1,9 +1,6 @@
 import math
-def divide_or_square(num:float)->float:
-    if num % 5 == 0:
-        return math.sqrt(num)
-    else:
-        return num % 5
+def divide_or_square(num:float) -> float:
+    return math.sqrt(num) if num % 5 == 0 else num % 5
 
 print("Divide of square challege 1:")
 print(divide_or_square(10))
