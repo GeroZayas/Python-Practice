f11ffdd6f0409a48df39c25cfe854d1d28d49145
gero zayas <gerozayas@gmail.com>
2023-06-20 16:32:21 +0200

    Update

--- pysimplegui_1.py
+++ pysimplegui_1.py
@@ -2,7 +2,7 @@ import PySimpleGUI as sg
 
 layout = [
     [sg.Text("Learn English Now!", background_color="darkred", font=("Arial", 30))],
-    [sg.Button(button_text="GO!", mouseover_colors="blue", button_color="darkgreen", font=("Arial", 14))],
+    [sg.Button(button_text="GO!", mouseover_colors="green", button_color="darkgreen", font=("Arial", 14))],
 ]
 
 window = sg.Window(
