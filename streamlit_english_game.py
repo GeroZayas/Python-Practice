7169076ecd11de6495d91845e24444ff55f01184
gero zayas <gerozayas@gmail.com>
2023-06-23 00:05:31 +0200

    Update

--- streamlit_english_game.py
+++ streamlit_english_game.py
@@ -6,24 +6,21 @@ st.set_page_config(layout="centered", page_title="English Vocabulary Game")
 
 # Insert Image in the Page
 from PIL import Image
+
 image = Image.open("./images/english.jpeg")
 st.image(image, width=400)
 # WELCOME MESSAGE
-st.markdown("""
+st.markdown(
+    """
 # English *Vocabulary* Game
 #### By Gero Zayas
-            """)
+            """
+)
 
-words = [
-    "Dog",
-    "Rabbit",
-    "Bat",
-    "Horse",
-    "Cat",
-]
+words = open("./words.txt", "r").read().split("\n")
 
-if question := st.text_input(f"How do you say **{choice(words)}** in Spanish?"):
-    if question == "Paris":
-        st.write("Correct!")
-    else:
-        st.write("Try again!")
+"## How do you say?"
+
+
+for word in words:
+    st.button(word,help="Click to see the translation")
