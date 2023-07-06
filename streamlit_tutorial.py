270e8730e591bb60769399372985bb660f7ae91d
gero zayas <gerozayas@gmail.com>
2023-07-06 12:52:40 +0200

    Delete components tutorial

--- streamlit_tutorial.py
+++ streamlit_tutorial.py
@@ -106,5 +106,19 @@ st.line_chart(chart_data)
 
 st.write("---")
 
-#################            ############################
+################# LATEX ############################
 
+st.header("st.latex")
+
+st.latex(
+    r"""
+    a + ar + a r^2 + a r^3 + \cdots + a r^{n-1} =
+    \sum_{k=0}^{n-1} ar^k =
+    a \left(\frac{1-r^{n}}{1-r}\right)
+    """
+)
+
+st.write("---")
+
+################# SECRETS ############################
+st.title("Secrets")
