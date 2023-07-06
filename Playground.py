58c459d1b14b6419a376209a44f5ccddd201559c
gero zayas <gerozayas@gmail.com>
2023-07-06 08:03:03 +0200

    Delete repeated files

--- Playground.py
+++ Playground.py
@@ -1,26 +1,7 @@
 import streamlit as st
-from streamlit_modal import Modal
 
-import streamlit.components.v1 as components
+st.header("st.selectbox")
 
+option = st.selectbox("What is your favorite color?", ("Blue", "Red", "Green"))
 
-modal = Modal("Demo Modal", "Open modal")
-if open_modal := st.button("Open"):
-    modal.open()
-
-if modal.is_open():
-    with modal.container():
-        st.write("Text goes here")
-
-        html_string = '''
-        <h1>HTML string in RED</h1>
-
-        <script language="javascript">
-          document.querySelector("h1").style.color = "red";
-        </script>
-        '''
-        components.html(html_string)
-
-        st.write("Some fancy text")
-        value = st.checkbox("Check me")
-        st.write(f"Checkbox checked: {value}")
+st.write("Your favorite color is ", option)
