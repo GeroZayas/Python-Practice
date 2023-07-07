2a9f2ed244789d393bc132c6c4e6a93b6574993b
gero zayas <gerozayas@gmail.com>
2023-07-07 11:04:39 +0200

    Add Info and Expander  widgets

--- Playground.py
+++ Playground.py
@@ -1,40 +1,9 @@
 import streamlit as st
 
-st.title("st.session_state")
-
-
-def lbs_to_kg():
-    st.session_state.kg = st.session_state.lbs / 2.2046
-
-
-def kg_to_lbs():
-    st.session_state.lbs = st.session_state.kg * 2.2046
-
-
-st.header("Input")
-col1, spacer, col2 = st.columns([3, 1, 3])
-with col1:
-    pounds = st.number_input("Pounds:", key="lbs", on_change=lbs_to_kg)
-with col2:
-    kilogram = st.number_input("Kilograms:", key="kg", on_change=kg_to_lbs)
-
-st.session_state.name, st.session_state.age = 'Gero Zayas', 32
-
-ss = st.session_state
-
-ss.family = [
-    'Sonia',
-    'Basilio',
-    'Carly',
-    ['More People',
-    'Some More People',
-    ['uno', 'dos', 'tres'],
-    ],
-    ]
-
-printeame_esto = st.write
-
-st.header("Output")
-st.write("st.session_state object:", st.session_state)
-
-printeame_esto("My life is great!")
+st.info('This is a purely informational message', icon="📡")
+with st.expander('See explanation'):
+    st.write("""
+        The chart above shows some numbers I picked for you.
+        I rolled actual dice for these, so they're *guaranteed* to
+        be random.
+    """)
