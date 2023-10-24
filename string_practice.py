b2c9c0714d925927c973378309549c58a9325032
Gero Zayas <gerozayas@gmail.com>
2023-10-24 06:34:42 +0200

    Initial

--- string_practice.py
+++ string_practice.py
@@ -2,3 +2,12 @@ import string
 
 letter = list(string.ascii_letters)
 print(f"==>> letter: {letter}")
+
+##########################################
+
+lower_letters = list(string.ascii_lowercase)
+print(f"==>> lower_letters: {lower_letters}")
+
+##########################################
+
+the_digits = (string.digits)
