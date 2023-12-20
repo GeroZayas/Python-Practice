82749898cddf922c5db56964c99b22a28f9a8b29
Gero Zayas <gerozayas@gmail.com>
2023-12-20 07:21:28 +0100

    Update

--- Playground.py
+++ Playground.py
@@ -1,8 +1,24 @@
 import collections
 
 fullname = collections.namedtuple(
-    typename='Full_name', field_names=['name', 'lastname']
+    typename="Full_name", field_names=["name", "lastname"]
 )
 
 gero_zayas = fullname("Gero", "Zayas")
 print(gero_zayas.name, gero_zayas.lastname)
+
+print(fullname._fields)
+
+# -------------------------------------------------
+
+d = collections.deque([])
+
+d.append("Gero")
+d.append("Mar")
+d.append(32)
+d.extend(['Abuelo', 'Abuela', 45])
+d.appendleft("Enrique")
+
+print(d)
+d.rotate(2)
+print(d)
