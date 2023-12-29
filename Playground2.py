d8c8d4c91271af76c499fdc4ff662d5c6fafbdae
Gero Zayas <gerozayas@gmail.com>
2023-12-29 09:35:57 +0100

    update

--- Playground2.py
+++ Playground2.py
@@ -1,31 +1,28 @@
-from itertools import permutations
-
-
-def middle_permutation(string: str):
-    sorted_string = sorted(string)
-    r = list(permutations(sorted_string))
-    middle_perm = "".join(r[int(len(r) / 2) - 1])
-    return middle_perm
-
-
-"""
-The problem here is when long strings are added, permutations gets exponentially bigger
-How to make more efficiently? namely, how not to actually get all the permutations
-just the "necessary" ones to get the wanted result
-"""
+def middle_permutation(string):
+    s = sorted(string)
+    if len(s) % 2 == 0:
+        middle_letter = s[len(s) // 2 - 1]
+        print(f"this is s[::-1] {s[::-1]}")
+        print(f"this is middle_letter if even {middle_letter}")
+        return s.pop(len(s) // 2 - 1) + "".join(s[::-1])
+    else:
+        return s.pop(len(s) // 2) + middle_permutation(s)
 
 
 s1 = "abc"
 s2 = "abcd"
 s3 = "abcdx"
-s4 = "cfryuiljkxns"
+s4 = "abcdxg"
+s5 = "dczxgba"
 
 r1 = middle_permutation(s1)
 r2 = middle_permutation(s2)
 r3 = middle_permutation(s3)
 r4 = middle_permutation(s4)
+r5 = middle_permutation(s5)
 
 print(r1)
 print(r2)
 print(r3)
 print(r4)
+print(r5)
