6406a443548ca2b4552db032082a5e97bac3a30a
Gero Zayas <gerozayas@gmail.com>
2023-12-31 18:59:07 +0100

    update

--- Playground.py
+++ Playground.py
@@ -1,37 +1,14 @@
-"""
-Task
+from typing import List, Tuple
 
-You are given a string s. Every letter in s appears once.
+def findPair(nums: List[int], target: int)-> Tuple[int]:
+    for i, n in enumerate(nums):
+        if target - n in nums and nums[]target - n:
+            return (n, target - n)
+    return ()
 
-Consider all strings formed by rearranging the letters in s. After ordering these strings in dictionary order, return the middle term. (If the sequence has a even length n, define its middle term to be the (n/2)th term.)
-Example
-
-For s = "abc", the result should be "bac".
-
- The permutations in order are: "abc", "acb", "bac", "bca", "cab", "cba" So, The middle term is "bac".
-Input/Output
-
-    [input] string s
-
-unique letters (2 <= length <= 26)
-
-    [output] a string
-
-middle permutation.
-"""
-from itertools import permutations
-
-s = "abc"
-s2 = "abcd"
-s3 = "abcde"
+nums = [5, 2, 6, 8, 1, 9] 
+target = 12
 
 
-def middle_permutation(string: str):
-    r = list(permutations(string))
-    print(r)
-    middle_perm = ...
-    return middle_perm
-
-
-for s in [s, s2, s3]:
-    print(middle_permutation(s), end="\n\n\n")
+r = findPair(nums, target)
+print(r)
