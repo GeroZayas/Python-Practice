84297d6ff9429442b2ac45322030559774b99354
Gero Zayas <gerozayas@gmail.com>
2024-01-02 06:48:41 +0100

    Update

--- Playground2.py
+++ Playground2.py
@@ -1,18 +1,1 @@
-# my_str = "Gero is an amazing programmer and he really enjoys it"
 
-# encoded_str = my_str.encode()
-
-# print(encoded_str)
-# print(type(encoded_str))
-# print(type(my_str))
-
-people = "Gero Mar Elisa Enrique Migdalia Sonia Basilio Zayas".split()
-print(people)
-
-people_iter = iter(people)
-print(people_iter.__next__())
-print(people_iter.__next__())
-print(people_iter.__next__())
-print(people_iter.__next__())
-print(people_iter.__next__())
-print(people_iter.__next__())
