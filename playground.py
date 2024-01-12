4d23f1b35be897b77bc7e245867dc48fdabd93cf
Gero Zayas <gerozayas@gmail.com>
2024-01-12 07:42:07 +0100

    Update

--- playground.py
+++ playground.py
@@ -1,32 +1,26 @@
-class TreeNode:
-    def __init__(self, value):
-        self.value = value
-        self.children = []
+class Human:
+    def __init__(self, name, age) -> None:
+        self.name = name
+        self.age = age
 
-    def add_child(self, child_node):
-        self.children.append(child_node)
-    
-    def __repr__(self) -> str:
-        return " ".join(str(c.value) for c in self.children)
+    def works(self, work: bool):
+        return "Yes, I work" if work else "No, I dont"
 
-# Example of creating a tree
-root = TreeNode(1)
-child1 = TreeNode(2)
-child2 = TreeNode(3)
 
-root.add_child(child1)
-root.add_child(child2)
+class Engineer(Human):
+    def __init__(self, name, age) -> None:
+        super().__init__(name, age)
 
-child1.add_child(TreeNode(4))
-child1.add_child(TreeNode(5))
+    def works(self):
+        return "Always working"
 
-child2.add_child(TreeNode(6))
 
-# Visualizing the tree structure
-#        1
-#       / \
-#      2   3
-#     / \   \
-#    4   5   6
+gero = Engineer(name="Gero", age=32)
 
-print(root)
+mar = Human("Mar", 28)
+
+print("This is Gero")
+print(gero.name, gero.age, gero.works())
+
+print("This is Mar")
+print(mar.name, mar.age, mar.works(False))
