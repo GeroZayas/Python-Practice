e6b3192e973bf0ec3a9a44ec9995928bafa7d847
Gero Zayas <gerozayas@gmail.com>
2024-01-15 14:09:14 +0100

    Update

--- playground.py
+++ playground.py
@@ -1,23 +1,21 @@
-from sys import getsizeof
+def bs(nums:list, target:int):
+    left, right = 0, len(nums) - 1
+    while left < right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return f"Element found at index {mid}"
+        elif target < nums[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return f"Element {target} not found in array"
 
-# we create a list and then check the size of it
-my_list = "Gero Mar Elisa Enrique Migdalia Sonia".split()
+target = 56
 
-print(f"This is my list\n{my_list}")
-print("This is the size of my_list")
-print(getsizeof(my_list))  # 152
+numbers = [2,43,67,43,89,56,23,21,34]
+sorted_nums = sorted(numbers)
+print(sorted_nums)
 
-# now we append two more names to the list to
-# see how it changes
-my_list.extend("Basilio Zayas".split())
-
-print(f"This is my list now\n{my_list}")
-print("This is the size of my_list now")
-print(getsizeof(my_list))  # the same 152
-
-# we add more names now
-my_list.extend("Karla Carly Carlos Tania Gissi Eduardito Vero".split())
-
-print(f"This is my list now\n{my_list}")
-print("This is the size of my_list now")
-print(getsizeof(my_list))  # 184 -> it grew
+print("Apply binary search:")
+r = bs(sorted_nums, target)
+print(r)
