048d69c9f4877b514c3723e887cd2b8a53a8776f
Gero Zayas <gerozayas@gmail.com>
2024-02-03 07:33:22 +0100

    Update

--- Faker_Practice.py
+++ Faker_Practice.py
@@ -3,15 +3,19 @@
 # get random words from faker module
 import faker
 
+
 # 20 words from faker module
 def get_names(num_words):
     return [faker.Faker().name() for _ in range(num_words)]
 
+
 # print(get_words(20))
+
 
 # function gent random ages from 1 to 100
 def get_age(num):
     return [faker.Faker().random_int(min=1, max=100) for _ in range(num)]
+
 
 # print(get_age(20))
 
