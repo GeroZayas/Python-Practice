77409fd957011f9dc5a49c0fa772c27056a3cc26
Gero Zayas <gerozayas@gmail.com>
2024-02-24 07:58:07 +0100

    Update

---
+++ CODING_INTERVIEW_UNIVERSITY/Heap_Practice_HeapQ.py
@@ -0,0 +1,21 @@
+import heapq
+
+my_array = [3, 6, 5, 2, 8, 1, 9, 10]
+
+
+def separator():
+    print("=" * 80)
+
+
+separator()
+print(dir(heapq))
+separator()
+
+heapq.heapify(my_array)
+
+print(my_array)
+heapq.heappop(my_array)
+print(my_array)
+
+print(f"The 2 nmallest are {heapq.nsmallest(2, my_array)}")
+print(f"The 2 nlargest are {heapq.nlargest(2, my_array)}")
