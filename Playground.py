e4a02e44dd560e11d6c0f5fcae8b1f6f4b086757
Gero Zayas <gerozayas@gmail.com>
2024-04-10 08:48:24 +0200

    Add unittest

--- Playground.py
+++ Playground.py
@@ -1,4 +1,14 @@
-x, y, z, n = [int(input()) for _ in range(4)]
-result = [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
-print(result)
+import unittest
 
+
+def generate_coordinates(x, y, z, n):
+    return [[i, j, k] for i in range(x+1) for j in range(y+1) for k in range(z+1) if i+j+k != n]
+
+class TestCoordinatesGeneration(unittest.TestCase):
+    def test_generate_coordinates(self):
+        expected_result = [[0, 0, 0], [0, 0, 1], [0, 1, 0], [1, 0, 0], [1, 1, 1]]
+        self.assertEqual(generate_coordinates(1,1,1,2), expected_result)
+
+        
+if __name__ == "__main__":
+    unittest.main()
