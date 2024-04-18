5b075daaae918f4fa5306eb12a806024256b3bfb
Gero Zayas <gerozayas@gmail.com>
2024-04-18 07:38:22 +0200

    Add heapq

--- CODING_INTERVIEW_UNIVERSITY/pg.py
+++ CODING_INTERVIEW_UNIVERSITY/pg.py
@@ -2,11 +2,18 @@ import heapq
 
 import random
 
+print(heapq)
+
 # Generate a list of 10 random numbers between 1 and 100
-random_numbers = [random.randint(1, 100) for _ in range(10)]
+random_numbers = [45,67,23,43,12,32,22]
 
 # Generate a list of 5 random names
 random_names = [''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(5, 10))) for _ in range(5)]
 
 print("Random numbers:", random_numbers)
 print("Random names:", random_names)
+
+heapq.heapify(random_numbers)
+
+print("Heapq random numbers:")
+print(random_numbers)
