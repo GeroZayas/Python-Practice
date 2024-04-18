f084115320a26a4369f99a57090dfde204927cf2
Gero Zayas <gerozayas@gmail.com>
2024-04-18 07:34:14 +0200

    Add Random names and numbers

--- CODING_INTERVIEW_UNIVERSITY/pg.py
+++ CODING_INTERVIEW_UNIVERSITY/pg.py
@@ -1,2 +1,12 @@
 import heapq
 
+import random
+
+# Generate a list of 10 random numbers between 1 and 100
+random_numbers = [random.randint(1, 100) for _ in range(10)]
+
+# Generate a list of 5 random names
+random_names = [''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(5, 10))) for _ in range(5)]
+
+print("Random numbers:", random_numbers)
+print("Random names:", random_names)
