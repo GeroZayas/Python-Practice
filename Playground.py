59ff5e20969cb0f8a67337bf2a92ce90d3113d80
Gero Zayas <gerozayas@gmail.com>
2024-04-19 10:18:47 +0200

    Add unittest for square function

---
+++ Playground.py
@@ -0,0 +1,21 @@
+import unittest
+
+
+def square(x):
+    # print(f"The square of {x} is:")
+    return x * 2
+
+
+print(square(4))
+
+
+class TestSquare(unittest.TestCase):
+    def test_square_first(self):
+        self.assertEqual(first=square(4), msg="TESTING MESSAGE", second=16)
+
+    def test_square_second(self):
+        self.assertEqual(first=square(8), msg="TESTING MESSAGE NUMBER 2", second=64)
+
+
+if __name__ == "__main__":
+    unittest.main()
