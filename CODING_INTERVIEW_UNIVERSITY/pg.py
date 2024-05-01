8838a33cbf5c0c39849b793d8fc67d24e96e9fba
Gero Zayas <gerozayas@gmail.com>
2024-05-01 08:35:59 +0200

    Update

--- CODING_INTERVIEW_UNIVERSITY/pg.py
+++ CODING_INTERVIEW_UNIVERSITY/pg.py
@@ -8,7 +8,7 @@ print(heapq)
 random_numbers = [45,67,23,43,12,32,22]
 
 # Generate a list of 5 random names
-random_names = [''.join(random.choices('abcdefghijklmnopqrstuvwxyz', k=random.randint(5, 10))) for _ in range(5)]
+random_names = [''.join(random.choices('bacategofutru', k=random.randint(5, 10))) for _ in range(5)]
 
 print("Random numbers:", random_numbers)
 print("Random names:", random_names)
