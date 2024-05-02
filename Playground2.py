357e3f9dd5b690aaa0db1d53f1c7a8e920d87aca
Gero Zayas <gerozayas@gmail.com>
2024-05-02 11:28:14 +0200

    Try asyncio

--- Playground2.py
+++ Playground2.py
@@ -1,40 +1,19 @@
-import heapq
-# from random import randint 
-
-# numbers = [randint(1,30) for _ in range(6)]
-numbers = [2, 1, 23, 5, 11, 24]
+import asyncio
+import aiohttp
 
 
-def separator():
-    print("-" * 50)
-
-print("Before heapify:")
-print(numbers)
-
-heapq.heapify(numbers)
+async def fetch(session, url):
+    async with session.get(url) as response:
+        return await response.text()
 
 
-separator()
-print("After heapify Min Heap:")
-print(numbers)
-
-print("Popping from the Min Heap")
-print(heapq.heappop(numbers))
+async def main(urls):
+    async with aiohttp.ClientSession() as session:
+        tasks = [fetch(session, url) for url in urls]
+        return await asyncio.gather(*tasks)
 
 
-separator()
+urls = ["http://www.gerozayas.com", "http://best-english-resources.pages.dev"]
+result = asyncio.run(main(urls))
 
-print("Let's make it a MAx Heap")
-numbers = [-n for n in numbers]
-heapq.heapify(numbers)
-numbers = [abs(n) for n in numbers]
-
-print(numbers)
-
-print("3 Largest numbers")
-print(heapq.nlargest(3, numbers))
-print("3 smallest numbers")
-print(heapq.nsmallest(3, numbers))
-
-print("Popping from the Max Heap")
-print(heapq.heappop(numbers))
+# print(result)
