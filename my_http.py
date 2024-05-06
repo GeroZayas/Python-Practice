1491538a98c37aed9f87f2e9ac6c5b36696137c3
Gero Zayas <gerozayas@gmail.com>
2024-05-06 07:36:31 +0200

    Add my http file

---
+++ my_http.py
@@ -0,0 +1,11 @@
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from random import choice
+
+class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Hello, Gero')
+
+httpd = HTTPServer(('localhost', 8000), SimpleHTTPRequestHandler)
+httpd.serve_forever()
