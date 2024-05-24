73c9d63e8a4a7bed372eb8f990c02b9ef792cefd
Gero Zayas <gerozayas@gmail.com>
2024-05-24 10:48:30 +0200

    Update

--- PyProjectPractice/python_boilerplate_gero/docs/usage.rst
+++
@@ -1,7 +0,0 @@
-=====
-Usage
-=====
-
-To use Python Boilerplate Gero in a project::
-
-    import python_boilerplate_gero
--- PyProjectPractice/python_boilerplate_gero/src/python_boilerplate_gero/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Top-level package for Python Boilerplate Gero."""
-
-__author__ = """Gero Zayas"""
-__email__ = 'gerozayas@gmail.com'
-__version__ = '0.1.0'
