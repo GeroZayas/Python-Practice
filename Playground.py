d0c6c36516f83b15f97085694f64ba7e8a811a50
Gero Zayas <gerozayas@gmail.com>
2024-05-24 11:34:06 +0200

    Added EnglishTeacher subclass to Professions module.

--- Playground.py
+++ Playground.py
@@ -1,6 +1,6 @@
 # The class `Professions` in Python defines attributes for name, salary, and experience of a
 # profession.
-from professions.professions import Professions, Teacher
+from professions.professions import Professions, Teacher, EnglishTeacher
 
 firefighter = Professions("Firefighter", 24000, "A lot")
 
@@ -13,3 +13,14 @@ print(answer)
 teacher = Teacher(name="Elisa", salary=30000, experience="Lots", subject="Medicine")
 print(teacher)
 print(teacher.is_working(True))
+
+
+english_teacher = EnglishTeacher(
+    name="Gerardito",
+    subject="ESL",
+    experience="Many years",
+    certificates="CAE - ADVANCED",
+    salary=45000,
+)
+
+print(english_teacher)
