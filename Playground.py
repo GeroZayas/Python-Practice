16b3762a90fd7c32cdb4bc86c47482a073ccc837
Gero Zayas <gerozayas@gmail.com>
2024-05-24 11:00:31 +0200

    Update professions

--- Playground.py
+++ Playground.py
@@ -1,11 +1,15 @@
 # The class `Professions` in Python defines attributes for name, salary, and experience of a
 # profession.
-from professions.professions import Professions
+from professions.professions import Professions, Teacher
 
-teacher = Professions("Teacher", 24000, "A lot")
+firefighter = Professions("Firefighter", 24000, "A lot")
 
-print(teacher)
+print(firefighter)
 
-answer = teacher.is_working(False)
+answer = firefighter.is_working(False)
 
 print(answer)
+
+teacher = Teacher(name="Elisa", salary=30000, experience="Lots", subject="Medicine")
+print(teacher)
+print(teacher.is_working(True))
