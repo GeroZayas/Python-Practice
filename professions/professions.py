dd3d433268b508b8d9b1efb89ec2ea0859a43f61
Gero Zayas <gerozayas@gmail.com>
2024-05-24 11:00:31 +0200

    Update professions

--- professions/professions.py
+++ professions/professions.py
@@ -5,10 +5,23 @@ class Professions:
         self.experience = experience
 
     def __repr__(self) -> str:
+        print("---")
         return f"Name: {self.name}, Salary: {self.salary} and Experience: {self.experience}"
 
     def is_working(self, working_now: bool):
         return f"{self.name} Is it working now? -> {working_now}"
+
+
+class Teacher(Professions):
+    def __init__(self, name: str, salary: int, experience: str, subject: str):
+        super().__init__(name, salary, experience)
+        self.name = name
+        self.salary = salary
+        self.experience = experience
+        self.subject = subject
+
+    def __repr__(self) -> str:
+        return super().__repr__() + f" || Subject: {self.subject}"
 
 
 if __name__ == "__main__":
