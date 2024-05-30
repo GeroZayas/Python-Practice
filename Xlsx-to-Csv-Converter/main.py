eb77ec29b2cc1b9fffc69d851b501a26b4765c63
Gero Zayas <gerozayas@gmail.com>
2024-05-30 08:50:47 +0200

    Fix OUTPUT file

--- Xlsx-to-Csv-Converter/main.py
+++ Xlsx-to-Csv-Converter/main.py
@@ -23,7 +23,7 @@ def batch_convert_xlsx_to_csv(input_dir, output_csv_file):
             all_csv_content.extend(csv_content)
             print(f"Converted {xlsx_file}")
 
-    with open(output_csv_file, mode="w", newline="") as file:
+    with open(f"OUTPUT/{output_csv_file}", mode="w", newline="") as file:
         writer = csv.writer(file)
         for row in all_csv_content:
             writer.writerow(row)
@@ -32,7 +32,7 @@ def batch_convert_xlsx_to_csv(input_dir, output_csv_file):
 
 def main():
     input_dir = input("Enter the directory containing the XLSX files: ")
-    output_csv_file = input("Enter the path for the output large CSV file: ")
+    output_csv_file = input("Enter the name for the output large CSV file: ")
     batch_convert_xlsx_to_csv(input_dir, output_csv_file)
     print("Batch conversion and combining completed successfully.")
 
