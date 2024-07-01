a3fcc0b83287df2255af56cdc0ed5ba61bbc79c5
Gero Zayas <gerozayas@gmail.com>
2024-07-01 18:16:04 +0200

    "Commented out multiple Streamlit function calls and added a new st.json() call."

--- Streamlit/Streamlit_Practice.py
+++ Streamlit/Streamlit_Practice.py
@@ -1,17 +1,20 @@
 import streamlit as st
-import pandas as pd
-import numpy as np
+
+# import pandas as pd
+# import numpy as np
 
 
-st.write("Most objects")  # df, err, func, keras!
-st.write(["st", "is <", 3])  # see *
+# st.write("Most objects")  # df, err, func, keras!
+# st.write(["st", "is <", 3])  # see *
 
-st.text("Fixed width text")
-st.markdown("_Markdown_")  # see *
-st.latex(r""" e^{i\pi} + 1 = 0 """)
-st.title("My title")
-st.header("My header")
-st.subheader("My sub")
-st.code("for i in range(8): foo()")
-st.html("<p>Hi!</p>")
-st.html("<h1>Hi!</h1>")
+# st.text("Fixed width text")
+# st.markdown("_Markdown_")  # see *
+# st.latex(r""" e^{i\pi} + 1 = 0 """)
+# st.title("My title")
+# st.header("My header")
+# st.subheader("My sub")
+# st.code("for i in range(8): foo()")
+# st.html("<p>Hi!</p>")
+# st.html("<h1>Hi!</h1>")
+
+st.json({"foo": "bar", "fu": "ba"})
