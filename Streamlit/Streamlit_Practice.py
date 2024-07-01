9e0a00a0adda5de74670fd143f2c686df9969a45
Gero Zayas <gerozayas@gmail.com>
2024-07-01 15:51:15 +0200

    "Added various Streamlit API calls to demonstrate different output formats"

--- Streamlit/Streamlit_Practice.py
+++ Streamlit/Streamlit_Practice.py
@@ -1,3 +1,17 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+
+
+st.write("Most objects")  # df, err, func, keras!
+st.write(["st", "is <", 3])  # see *
+
+st.text("Fixed width text")
+st.markdown("_Markdown_")  # see *
+st.latex(r""" e^{i\pi} + 1 = 0 """)
+st.title("My title")
+st.header("My header")
+st.subheader("My sub")
+st.code("for i in range(8): foo()")
+st.html("<p>Hi!</p>")
+st.html("<h1>Hi!</h1>")
