209d7511502cd0de9bff3fa97cb3ffdafbb4a1d5
Gero Zayas <gerozayas@gmail.com>
2024-07-01 15:49:16 +0200

    "Removed most of the code in Streamlit_Practice.py, leaving only the import statements."

--- Streamlit/Streamlit_Practice.py
+++ Streamlit/Streamlit_Practice.py
@@ -1,33 +1,3 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
-st.title("Streamlit Practice")
-
-data = {
-    "1": "one",
-    "2": "two",
-    "3": "three",
-    "4": "four",
-}
-question_container = st.container()
-
-st.write("### Hello, *World!* :sunglasses:")
-
-df = pd.DataFrame(
-    {
-        "Name": ["Geronimo", "Pocahontas", "Sitting Bull", "Crazy Horse"],
-        "Job": ["Doctor", "Lawyer", "Teacher", "Engineer"],
-    }
-)
-st.write(df)
-
-
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-data = np.random.randn(10, 1)
-
-tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
-
-tab2.subheader("A tab with the data")
-tab2.write(data)
