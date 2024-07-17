f4baf53a7440e96d908aa689172e33d37e1c7a4c
Gero Zayas <gerozayas@gmail.com>
2024-07-17 15:16:19 +0200

    "Modified Playground.py to create and print a namedtuple instance; updated questions_and_answers.pptx"

--- Playground.py
+++ Playground.py
@@ -4,4 +4,10 @@ nt = collections.namedtuple
 
 people = nt("People", ["name", "age", "job"])
 
-print(people)
+# print(people)
+
+gero = people("Gero", 33, "Programmer")
+
+print(gero.name)
+print(gero.age)
+print(gero.job)
