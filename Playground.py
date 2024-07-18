6de74cf1c956806719c20272d89967a665f497b0
Gero Zayas <gerozayas@gmail.com>
2024-07-18 15:53:47 +0200

    Update People in Playground

--- Playground.py
+++ Playground.py
@@ -11,3 +11,13 @@ gero = people("Gero", 33, "Programmer")
 print(gero.name)
 print(gero.age)
 print(gero.job)
+
+people_dict = {
+    "person1": ["Mar", 33, "HR"],
+    "person2": ["Cristina", 60, "Optometrist"],
+}
+
+print(people_dict)
+
+for p in people_dict.items():
+    print(p[1])
