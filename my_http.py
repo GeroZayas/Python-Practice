d6f57af3f449448f4af19a632db412f817b86bfb
Gero Zayas <gerozayas@gmail.com>
2024-07-22 09:37:24 +0200

    "Updated my_http.py to write additional lines to response, and modified questions_and_answers.pptx"

--- my_http.py
+++ my_http.py
@@ -5,7 +5,9 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
         self.end_headers()
-        self.wfile.write(b"Hello, Gero")
+        self.wfile.write(b"Hello, Gero\n")
+        self.wfile.write(b"Hello, Again\n")
+        self.wfile.write("<h1>Hello, Again</h1>\n")
 
 
 httpd = HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
