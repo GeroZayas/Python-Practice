7e9c34b5ecd149c26239bd10e1121ef7434bf06b
Gero Zayas <gerozayas@gmail.com>
2024-07-25 08:48:33 +0200

    "Added test cases to Contains Duplicate.py and modified questions_and_answers.pptx"

--- NeetCodeIO/Contains Duplicate.py
+++ NeetCodeIO/Contains Duplicate.py
@@ -10,3 +10,11 @@ class Solution:
                 return True
             hashset.add(n)
         return False
+
+
+nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 1]
+nums_2 = [4, 5, 6, 7, 8, 9]
+
+solution = Solution()
+print(solution.containsDuplicate(nums))  # Output: True
+print(solution.containsDuplicate(nums_2))  # Output: False
