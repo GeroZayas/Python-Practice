a0a73060b44ee32b8f3bdbb13824f0e5a8b8ba59
Gero Zayas <gerozayas@gmail.com>
2024-08-26 09:38:00 +0100

    Add min stack

---
+++ stack_min_stack_neetcode.py
@@ -0,0 +1,44 @@
+import marimo
+
+__generated_with = "0.6.19"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __():
+    class MinStack:
+        def __init__(self):
+            self.stack = []
+            self.minStack = []
+
+        def push(self, val: int) -> None:
+            self.stack.append(val)
+            val = min(val, self.minStack[-1] if self.minStack else val)
+            self.minStack.append(val)
+
+        def pop(self) -> None:
+            self.stack.pop()
+            self.minStack.pop()
+
+        def top(self) -> int:
+            return self.stack[-1]
+
+        def getMin(self) -> int:
+            return self.minStack[-1]
+
+    return MinStack,
+
+
+@app.cell
+def __(MinStack):
+    minStack = MinStack()
+    return minStack,
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
