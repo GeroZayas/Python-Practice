69e29b7a2d13b2bb864553b17c9a1b16c6765fe1
Gero Zayas <gerozayas@gmail.com>
2024-08-27 07:15:00 +0100

    Add evaluate reverse polish

---
+++ stack_evaluate_reverse_polish_neetcode.py
@@ -0,0 +1,42 @@
+import marimo
+
+__generated_with = "0.6.19"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __(List):
+    class Solution:
+        def evalRPN(self, tokens: List[str]) -> int:
+            stack = []
+            for c in tokens:
+                if c == "+":
+                    stack.append(stack.pop() + stack.pop())
+                elif c == "-":
+                    a, b = stack.pop(), stack.pop()
+                    stack.append(b - a)
+                elif c == "*":
+                    stack.append(stack.pop() * stack.pop())
+                elif c == "/":
+                    a, b = stack.pop(), stack.pop()
+                    stack.append(int(float(b) / a))
+                else:
+                    stack.append(int(c))
+            return stack[0]
+
+    return Solution,
+
+
+@app.cell
+def __(Solution):
+    solution = Solution()
+    return solution,
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
