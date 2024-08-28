b9d625f9a7384f2e59688e7e0d51fae525d96701
Gero Zayas <gerozayas@gmail.com>
2024-08-28 12:49:00 +0100

    Add general parentheses0

---
+++ stack_general_parentheses_neetcode.py
@@ -0,0 +1,46 @@
+import marimo
+
+__generated_with = "0.6.19"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def __(List):
+    class Solution:
+        def generateParenthesis(self, n: int) -> List[str]:
+            stack = []
+            res = []
+
+            def backtrack(openN, closedN):
+                if openN == closedN == n:
+                    res.append("".join(stack))
+                    return
+
+                if openN < n:
+                    stack.append("(")
+                    backtrack(openN + 1, closedN)
+                    stack.pop()
+                if closedN < openN:
+                    stack.append(")")
+                    backtrack(openN, closedN + 1)
+                    stack.pop()
+
+            backtrack(0, 0)
+            return res
+
+    return Solution,
+
+
+@app.cell
+def __(Solution):
+    solution = Solution()
+    return solution,
+
+
+@app.cell
+def __():
+    return
+
+
+if __name__ == "__main__":
+    app.run()
