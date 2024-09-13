fd52354716471c5017b1196847f544482e384df3
Gero Zayas <gerozayas@gmail.com>
2024-09-13 10:00:34 +0200

    Add new algo

--- NeetCode.py
+++ NeetCode.py
@@ -7,3 +7,20 @@ class Solution:
                 return True
             hashset.add(n)
         return False
+
+
+# -------------------------------
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        countS, countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+
+    # easier solution
+    # return True if sorted(s) == sorted(t) else False
