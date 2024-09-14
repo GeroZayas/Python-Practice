ae732df9e1ffdc544923e416c686af9737faee4d
Gero Zayas <gerozayas@gmail.com>
2024-09-14 15:23:47 +0200

    Add algo

--- NeetCode.py
+++ NeetCode.py
@@ -24,3 +24,16 @@ class Solution:
 
     # easier solution
     # return True if sorted(s) == sorted(t) else False
+
+
+# -------------------------------
+class Solution:
+    def getConcatenation(self, nums: List[int]) -> List[int]:
+        ans = []
+        for i in range(2):
+            for n in nums:
+                ans.append(n)
+        return ans
+
+
+# -------------------------------
