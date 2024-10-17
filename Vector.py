ad3af9ec647a23ab7e57465036b13487acbe5ea2
Gero Zayas <gerozayas@gmail.com>
2024-10-17 07:58:20 +0200

    Update typing

--- Vector.py
+++ Vector.py
@@ -1,18 +1,20 @@
 class Vector:
     def __init__(self) -> None:
-        self._data = []  # create a list for def funcname(self, parameter_list):
+        self._data: list[int | float] = (
+            []
+        )  # create a list for def funcname(self, parameter_list):
 
-    def append(self, value):
+    def append(self, value: float) -> None:
         self._data.append(value)
 
-    def remove(self, index):
+    def remove(self, index: int) -> None:
         del self._data[index]
 
-    def insert(self, index, value):
+    def insert(self, index: int, value: int | float) -> None:
         self._data.insert(index, value)
 
-    def get(self, index):
+    def get(self, index: int) -> int | float:  # -> Any:
         return self._data[index]
 
-    def size(self):
+    def size(self) -> int:
         return len(self._data)
