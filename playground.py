3b3928e5ee43fc5e4742425b3411779c85ea1050
Gero Zayas <gerozayas@gmail.com>
2024-10-23 16:38:47 +0200

    Add interactive coding challenges repo

--- playground.py
+++ playground.py
@@ -1,15 +1,3 @@
-import pandas as pd
-import numpy as np
+from itertools import tee
 
-data = pd.read_csv("./comercio_barcelona_2019.csv")
-
-data_head = data.head()
-data_columns = data.columns
-print("How many columns: ", len(data_columns))
-
-# print(data_columns)
-
-# for col in data_columns:
-#     print(f"\t {col}")
-
-print(data["Nom_Principal_Activitat"].unique())
+print("Hello")
