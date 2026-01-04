835942e7fde47e09ca357467a213756f13f96d6d
Gero Zayas <gerozayas@gmail.com>
2026-01-04 08:31:08 +0100

    update playground file

--- pg.py
+++ pg.py
@@ -1,34 +1,10 @@
-from io import StringIO 
-from icecream import ic
-import json
+from rich import print
 
-string_buffer = StringIO()
+def remove_falsy(unfiltered_list):
+    return list(filter(bool, unfiltered_list)) 
 
-initial_value_text = "This is a sentence for testing my string Io stuff."
-secondary_value_text = "THIS IS THE SECOND text value i will be writing to the buffer"
+my_list = ["hello", "my", 0, 0, 1, False, True, "name", "is", "", "", "", "you", "know"]
 
-ic(string_buffer.write(initial_value_text))
-string_buffer.seek(0)
+print(my_list)
 
-ic(string_buffer.read())
-
-string_buffer.writelines(secondary_value_text)
-string_buffer.write("\n\n\n")
-string_buffer.seek(0)
-ic(string_buffer.read())
-
-with open("./tinydb-practice/todo_db.json", "r") as f:
-    my_json_content = f.read()
-
-string_buffer.write(my_json_content) 
-ic(string_buffer.read())
-string_buffer.seek(0)
-ic(string_buffer.read())
-
-print("This is something completely new now, using print()", file=string_buffer)
-
-ic(string_buffer.seekable())
-
-string_buffer.seek(0)
-
-ic(string_buffer.read())
+print(remove_falsy(my_list))
